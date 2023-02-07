42dad4c0dd322b70539fb1910a8c95418adda60d
acd02 <alexandre.cadiot@adevinta.com>
2023-02-07 14:36:09 +0100

    chore: run "prettify" npm script

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -1,27 +1,28 @@
-import React from 'react';
-import './button.css';
+import './button.css'
+
+import React from 'react'
 
 interface ButtonProps {
   /**
    * Is this the principal call to action on the page?
    */
-  primary?: boolean;
+  primary?: boolean
   /**
    * What background color to use
    */
-  backgroundColor?: string;
+  backgroundColor?: string
   /**
    * How large should the button be?
    */
-  size?: 'small' | 'medium' | 'large';
+  size?: 'small' | 'medium' | 'large'
   /**
    * Button contents
    */
-  label: string;
+  label: string
   /**
    * Optional click handler
    */
-  onClick?: () => void;
+  onClick?: () => void
 }
 
 /**
@@ -34,7 +35,8 @@ export const Button = ({
   label,
   ...props
 }: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary'
+
   return (
     <button
       type="button"
@@ -44,5 +46,5 @@ export const Button = ({
     >
       {label}
     </button>
-  );
-};
+  )
+}
