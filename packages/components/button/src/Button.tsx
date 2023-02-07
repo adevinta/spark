df0a3484612f138c963047c7e94f643aedd4d31b
Andrés Alvarez Sordo <a.alvarez.sor@gmail.com>
2023-02-07 19:03:38 +0100

    refactor: remove not needed new lines

--- packages/components/button/src/Button.tsx
+++ packages/components/button/src/Button.tsx
@@ -1,11 +1,7 @@
-
-
 import { ComponentPropsWithoutRef, PropsWithChildren } from 'react'
 
-export interface ButtonProps extends ComponentPropsWithoutRef<'button'> {};
+export type ButtonProps = ComponentPropsWithoutRef<'button'>
 
 export function Button(props: PropsWithChildren<ButtonProps>) {
-  return (
-    <button {...props} />
-  )
+  return <button {...props} />
 }
