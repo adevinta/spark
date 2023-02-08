1c5ed746092339eaa047bbcc6f1a0135467ff18b
arnau-rius <arnau.rius.contact@gmail.com>
2023-02-08 09:38:40 +0100

    chore: add commitlint/config-lerna-scopes

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,1 +1,11 @@
-module.exports = {extends: ['@commitlint/config-conventional']}
+const lernaConfig = require('@commitlint/config-lerna-scopes')
+
+module.exports = {
+  extends: ['@commitlint/config-conventional', '@commitlint/config-lerna-scopes'],
+  rules: {
+    'scope-enum': async () => {
+      const pkgs = await lernaConfig.utils.getPackages()
+      return [2, 'always', ['root', 'release', ...pkgs]]
+    },
+  },
+}
