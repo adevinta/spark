e3ee6a49a94103ca191b19966b370aad406673d5
acd02 <alexandre.cadiot@adevinta.com>
2023-02-08 14:48:58 +0100

    feat: add tailwind

--- .storybook/preview.cjs
+++ .storybook/preview.cjs
@@ -1,5 +1,7 @@
+import '../src/tailwind.css'
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
