b1b14df0244efe8378480e219843825e557d3e9f
arnau-rius <arnau.rius.contact@gmail.com>
2023-02-08 12:50:42 +0100

    chore: not needed

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -2,10 +2,4 @@ const lernaConfig = require('@commitlint/config-lerna-scopes')
 
 module.exports = {
   extends: ['@commitlint/config-conventional', '@commitlint/config-lerna-scopes'],
-  rules: {
-    'scope-enum': async () => {
-      const pkgs = await lernaConfig.utils.getPackages()
-      return [2, 'always', ['root', ...pkgs]]
-    },
-  },
 }
