36d1475c112b242bd6afc4142d972a5c5cbfc47a
Kiko Ruiz Lloret <kikoruizlloret@gmail.com>
2023-02-09 16:06:05 +0100

    feat: remove button component for now

--- packages/components/button/stories/Button.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactLiveBlock } from '@docs/helpers/ReactLiveBlock'
-
-import { Button } from '../src/index'
-
-export const Default = () => (
-  <ReactLiveBlock scope={{ Button }}>
-    <Button>primary button</Button>
-  </ReactLiveBlock>
-)
