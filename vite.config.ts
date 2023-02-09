2ff326020587b7bcf8c23dc0d98b9b5a685c13fe
Powerplex <julien.bonnin.dev@gmail.com>
2023-02-09 11:09:40 +0100

    chore: setup coverage and vitest ui

--- vite.config.ts
+++ vite.config.ts
@@ -13,5 +13,8 @@ export default defineConfig({
     // you might want to disable it, if you don't have tests that rely on CSS
     // since parsing CSS is slow
     css: true,
+    coverage: {
+      provider: 'istanbul',
+    },
   },
 })
