82732503a1ca3b4e9d4372d5856d2110ec0a7d64
andresin87 <andresin87@gmail.com>
2023-02-14 15:35:35 +0100

    feat(use-mounted-state): add useMountedState react hook

---
+++ packages/hooks/use-mounted-state/src/useMountedState.tsx
@@ -0,0 +1,17 @@
+import { useCallback, useEffect, useRef } from 'react'
+
+export function useMountedState(): () => boolean {
+  const mountedRef = useRef<boolean>(false)
+
+  const get = useCallback(() => mountedRef.current, [])
+
+  useEffect(() => {
+    mountedRef.current = true
+
+    return () => {
+      mountedRef.current = false
+    }
+  }, [])
+
+  return get
+}
