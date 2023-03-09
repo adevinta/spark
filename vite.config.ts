eec2b08be6e33f7a42f3e0d62660d0f01fd77c29
arnau-rius <arnau.rius.contact@gmail.com>
2023-03-09 11:56:13 +0100

    refactor: remove log
    use browserlist to define build.target
    
    347

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import browserslistToEsbuild from 'browserslist-to-esbuild'
 
-console.log('CONFIG', browserslistToEsbuild())
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [tsconfigPaths(), react()],
