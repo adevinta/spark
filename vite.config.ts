7674960a78e01e9f3fd9860eb259932bf9a6de9f
arnau-rius <arnau.rius.contact@gmail.com>
2023-03-09 11:44:16 +0100

    chore: build target follows browserlist spec

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import browserslistToEsbuild from 'browserslist-to-esbuild'
 
+console.log('CONFIG', browserslistToEsbuild())
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [tsconfigPaths(), react()],
+  build: {
+    target: browserslistToEsbuild(),
+  },
   test: {
     globals: true,
     environment: 'jsdom',
