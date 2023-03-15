bfdf084a664dc1060889b2f78ee20c70c71c8bd9
Kiko Ruiz Lloret <kikoruizlloret@gmail.com>
2023-03-15 16:40:16 +0100

    feat(button): add first main combinations

--- packages/components/button/src/Button.styles.tsx
+++ packages/components/button/src/Button.styles.tsx
@@ -1,137 +1,183 @@
 import { cva, type VariantProps } from 'class-variance-authority'
-// import { paramCase } from 'param-case'
-
-// const BUTTON_INTENTS = [
-//   'primary',
-//   'primaryContainer',
-//   'primaryVariant',
-//   'onPrimary',
-//   'onPrimaryContainer',
-//   'onPrimaryVariant',
-//   'secondary',
-//   'secondaryContainer',
-//   'secondaryVariant',
-//   'onSecondary',
-//   'onSecondaryContainer',
-//   'onSecondaryVariant',
-// ]
-
-// function buildIntents() {
-//   return BUTTON_INTENTS.reduce(
-//     (intents, intent) => ({
-//       ...intents,
-//       [intent]: [
-//         `bg-${paramCase(intent)}`,
-//         `hover:bg-${paramCase(intent)}-hovered`,
-//         `disabled:bg-${paramCase(intent)}-disabled`,
-//         `focus:bg-${paramCase(intent)}-focused`,
-//       ],
-//     }),
-//     {}
-//   )
-// }
-
-// console.log({ intent: buildIntents() })
 
 const defaultVariants = {
   design: 'filled',
   intent: 'primary',
   size: 'md',
   reversed: false,
+  shape: 'rounded',
 } as const
 
-export const buttonStyles = cva('flex items-center justify-center', {
+export const buttonStyles = cva('flex items-center text-body-1 justify-center box-border', {
   variants: {
     design: {
-      filled: [],
-      outlined: [],
+      filled: [''],
+      outlined: ['ring-2', 'ring-current'],
       tinted: [],
       ghost: [],
     },
 
     intent: {
-      primary: [
-        'bg-primary',
-        'hover:bg-primary-hovered',
-        'disabled:bg-primary-disabled',
-        'focus:bg-primary-focused',
-      ],
-      primaryContainer: [
-        'bg-primary-container',
-        'hover:bg-primary-container-hovered',
-        'disabled:bg-primary-container-disabled',
-        'focus:bg-primary-container-focused',
-      ],
-      primaryVariant: [
-        'bg-primary-variant',
-        'hover:bg-primary-variant-hovered',
-        'disabled:bg-primary-variant-disabled',
-        'focus:bg-primary-variant-focused',
-      ],
-      onPrimary: [
-        'bg-on-primary',
-        'hover:bg-primary-hovered',
-        'disabled:bg-primary-disabled',
-        'focus:bg-primary-focused',
-      ],
-      onPrimaryContainer: [
-        'bg-primary-container',
-        'hover:bg-primary-container-hovered',
-        'disabled:bg-primary-container-disabled',
-        'focus:bg-primary-container-focused',
-      ],
-      onPrimaryVariant: [
-        'bg-primary-variant',
-        'hover:bg-primary-variant-hovered',
-        'disabled:bg-primary-variant-disabled',
-        'focus:bg-primary-variant-focused',
-      ],
-      secondary: [
-        'bg-secondary',
-        'hover:bg-secondary-hovered',
-        'disabled:bg-secondary-disabled',
-        'focus:bg-secondary-focused',
-      ],
-      secondaryContainer: [
-        'bg-secondary-container',
-        'hover:bg-secondary-container-hovered',
-        'disabled:bg-secondary-container-disabled',
-        'focus:bg-secondary-container-focused',
-      ],
-      secondaryVariant: [
-        'bg-secondary-variant',
-        'hover:bg-secondary-variant-hovered',
-        'disabled:bg-secondary-variant-disabled',
-        'focus:bg-secondary-variant-focused',
-      ],
-      onSecondary: [
-        'bg-on-secondary',
-        'hover:bg-secondary-hovered',
-        'disabled:bg-secondary-disabled',
-        'focus:bg-secondary-focused',
-      ],
-      onSecondaryContainer: [
-        'bg-secondary-container',
-        'hover:bg-secondary-container-hovered',
-        'disabled:bg-secondary-container-disabled',
-        'focus:bg-secondary-container-focused',
-      ],
-      onSecondaryVariant: [
-        'bg-secondary-variant',
-        'hover:bg-secondary-variant-hovered',
-        'disabled:bg-secondary-variant-disabled',
-        'focus:bg-secondary-variant-focused',
-      ],
+      primary: [],
+      primaryContainer: [],
+      primaryVariant: [],
+      secondary: [],
+      secondaryContainer: [],
+      secondaryVariant: [],
     },
     size: {
-      md: ['p-md'],
+      md: ['py-md px-lg'],
       lg: ['p-lg'],
     },
     reversed: {
       true: [],
     },
+    shape: {
+      rounded: ['rounded-lg'],
+      square: ['rounded-none'],
+      pill: ['rounded-full'],
+    },
   },
-  compoundVariants: [],
+  compoundVariants: [
+    {
+      intent: 'primary',
+      design: 'filled',
+      class: [
+        'bg-primary',
+        'hover:bg-primary-hovered',
+        'disabled:bg-primary-disabled',
+        'focus:bg-primary-focused',
+        'text-on-primary',
+      ],
+    } as const,
+    {
+      intent: 'primaryContainer',
+      design: 'filled',
+      class: [
+        'bg-primary-container',
+        'hover:bg-primary-container-hovered',
+        'disabled:bg-primary-container-disabled',
+        'focus:bg-primary-container-focused',
+        'text-on-primary-container',
+      ],
+    } as const,
+    {
+      intent: 'primaryVariant',
+      design: 'filled',
+      class: [
+        'bg-primary-variant',
+        'hover:bg-primary-variant-hovered',
+        'disabled:bg-primary-variant-disabled',
+        'focus:bg-primary-variant-focused',
+        'text-on-primary-variant',
+      ],
+    } as const,
+    {
+      intent: 'secondary',
+      design: 'filled',
+      class: [
+        'bg-secondary',
+        'hover:bg-secondary-hovered',
+        'disabled:bg-secondary-disabled',
+        'focus:bg-secondary-focused',
+        'text-on-secondary',
+      ],
+    } as const,
+    {
+      intent: 'secondaryContainer',
+      design: 'filled',
+      class: [
+        'bg-secondary-container',
+        'hover:bg-secondary-container-hovered',
+        'disabled:bg-secondary-container-disabled',
+        'focus:bg-secondary-container-focused',
+        'text-on-secondary-container',
+      ],
+    } as const,
+    {
+      intent: 'secondaryVariant',
+      design: 'filled',
+      class: [
+        'bg-secondary-variant',
+        'hover:bg-secondary-variant-hovered',
+        'disabled:bg-secondary-variant-disabled',
+        'focus:bg-secondary-variant-focused',
+        'text-on-secondary-variant',
+      ],
+    } as const,
+    {
+      intent: 'primary',
+      design: 'outlined',
+      class: [
+        // 'border-primary',
+        'bg-on-primary',
+        'hover:bg-primary-hovered',
+        'disabled:bg-primary-disabled',
+        'focus:bg-primary-focused',
+        'text-primary',
+      ],
+    } as const,
+    {
+      intent: 'primaryContainer',
+      design: 'outlined',
+      class: [
+        // 'border-primary-container',
+        'bg-on-primary-container',
+        'hover:bg-primary-container-hovered',
+        'disabled:bg-primary-container-disabled',
+        'focus:bg-primary-container-focused',
+        'text-primary-container',
+      ],
+    } as const,
+    {
+      intent: 'primaryVariant',
+      design: 'outlined',
+      class: [
+        // 'border-primary-variant',
+        'bg-on-primary-variant',
+        'hover:bg-primary-variant-hovered',
+        'disabled:bg-primary-variant-disabled',
+        'focus:bg-primary-variant-focused',
+        'text-primary-variant',
+      ],
+    } as const,
+    {
+      intent: 'secondary',
+      design: 'outlined',
+      class: [
+        // 'border-secondary',
+        'bg-on-secondary',
+        'hover:bg-secondary-hovered',
+        'disabled:bg-secondary-disabled',
+        'focus:bg-secondary-focused',
+        'text-secondary',
+      ],
+    } as const,
+    {
+      intent: 'secondaryContainer',
+      design: 'outlined',
+      class: [
+        // 'border-secondary-container',
+        'bg-on-secondary-container',
+        'hover:bg-secondary-container-hovered',
+        'disabled:bg-secondary-container-disabled',
+        'focus:bg-secondary-container-focused',
+        'text-secondary-container',
+      ],
+    } as const,
+    {
+      intent: 'secondaryVariant',
+      design: 'outlined',
+      class: [
+        // 'border-secondary-variant',
+        'bg-on-secondary-variant',
+        'hover:bg-secondary-variant-hovered',
+        'disabled:bg-secondary-variant-disabled',
+        'focus:bg-secondary-variant-focused',
+        'text-secondary-variant',
+      ],
+    } as const,
+  ],
   defaultVariants,
 })
 
