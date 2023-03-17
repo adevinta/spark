b585bf67c148551601a319cb263583a69acc101f
Andrés Alvarez Sordo <a.alvarez.sor@gmail.com>
2023-03-17 11:35:28 +0100

    chore: update deps

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,16 @@
 const themeConf = require('./tailwind.theme.cjs')
 const sparkPlugins = require('@spark-ui/tailwind-plugins')
+const tailwindcssRadix = require('tailwindcss-radix')
 
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
   theme: themeConf,
   content: ['./packages/**/*.{js,ts,jsx,tsx}', './documentation/**/*.{js,ts,jsx,tsx,mdx}'],
-  plugins: [sparkPlugins.animations()],
+  plugins: [
+    sparkPlugins.animations(),
+    tailwindcssRadix({
+      variantPrefix: 'spark',
+    }),
+  ],
 }
