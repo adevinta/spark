7009c513ab37b1118b89d7bada365156fb85f362
acd02 <alexandre.cadiot@adevinta.com>
2023-03-17 18:14:59 +0100

    feat(tailwind-plugins): add spark theme plugin
    Add Spark theme plugin
    
    #411

---
+++ packages/utils/tailwind-plugins/spark-theme/constants.js
@@ -0,0 +1,15 @@
+const tailwindCategoryKeys = {
+  colors: 'colors',
+  fontSize: 'fontSize',
+  screens: 'screens',
+}
+
+const unassignedColors = {
+  inherit: 'inherit',
+  current: 'currentColor',
+  transparent: 'transparent',
+}
+
+const DEFAULT_KEY = 'DEFAULT'
+
+module.exports = { tailwindCategoryKeys, unassignedColors, DEFAULT_KEY }
