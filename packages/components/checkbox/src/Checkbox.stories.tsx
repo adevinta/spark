00f7a0a43c502aa001e6c6d1fd10b04372daef7a
arnau-rius <arnau.rius.contact@gmail.com>
2023-03-20 14:27:18 +0100

    refactor(checkbox): use Canvas and StoryFn helpers

--- packages/components/checkbox/src/Checkbox.stories.tsx
+++ packages/components/checkbox/src/Checkbox.stories.tsx
@@ -1,59 +1,75 @@
-import { ReactLiveBlock } from '@docs/helpers/ReactLiveBlock'
+import { StoryFn } from '@storybook/react'
+import { useState } from 'react'
 
 import { Checkbox } from './Checkbox'
 
-export const Default = () => (
-  <ReactLiveBlock scope={{ Checkbox }}>
+export const Default: StoryFn = args => (
+  <div className="flex items-center gap-sm">
+    <Checkbox id={'c1'} {...args} />
+    <label
+      htmlFor={'c1'}
+      className="cursor-pointer pl-[4px] text-body-1 font-medium peer-disabled:cursor-not-allowed  peer-disabled:opacity-dim-3"
+    >
+      Accept terms and conditions.
+    </label>
+  </div>
+)
+
+export const Disabled: StoryFn = args => (
+  <div className="flex items-center gap-sm">
+    <Checkbox id={'c2'} disabled {...args} />
+    <label
+      htmlFor={'c2'}
+      className="cursor-pointer pl-[4px] text-body-1 font-medium peer-disabled:cursor-not-allowed  peer-disabled:opacity-dim-3"
+    >
+      Accept terms and conditions.
+    </label>
+  </div>
+)
+
+export const DefaultChecked: StoryFn = args => (
+  <div className="flex items-center gap-sm">
+    <Checkbox id={'c3'} defaultChecked {...args} />
+    <label
+      htmlFor={'c3'}
+      className="cursor-pointer pl-[4px] text-body-1 font-medium peer-disabled:cursor-not-allowed  peer-disabled:opacity-dim-3"
+    >
+      Accept terms and conditions.
+    </label>
+  </div>
+)
+
+export const Controlled: StoryFn = args => {
+  const [value, setValue] = useState(false)
+
+  const handleChange = (checked: boolean | 'indeterminate') => {
+    setValue(!!checked)
+  }
+
+  return (
     <div className="flex items-center gap-sm">
-      <Checkbox id={'c1'} />
+      <Checkbox id={'c4'} checked={value} onCheckedChange={handleChange} {...args} />
       <label
-        htmlFor={'c1'}
+        htmlFor={'c4'}
         className="cursor-pointer pl-[4px] text-body-1 font-medium peer-disabled:cursor-not-allowed  peer-disabled:opacity-dim-3"
       >
         Accept terms and conditions.
       </label>
     </div>
-  </ReactLiveBlock>
-)
+  )
+}
 
-export const Disabled = () => (
-  <ReactLiveBlock scope={{ Checkbox }}>
-    <div className="flex items-center gap-sm">
-      <Checkbox id={'c2'} disabled />
-      <label
-        htmlFor={'c2'}
-        className="cursor-pointer pl-[4px] text-body-1 font-medium peer-disabled:cursor-not-allowed  peer-disabled:opacity-dim-3"
-      >
-        Accept terms and conditions.
-      </label>
-    </div>
-  </ReactLiveBlock>
-)
+const intent = ['primary', 'secondary', 'success', 'alert', 'error', 'info', 'neutral'] as const
 
-export const DefaultChecked = () => (
-  <ReactLiveBlock scope={{ Checkbox }}>
-    <div className="flex items-center gap-sm">
-      <Checkbox id={'c3'} defaultChecked />
-      <label
-        htmlFor={'c3'}
-        className="cursor-pointer pl-[4px] text-body-1 font-medium peer-disabled:cursor-not-allowed  peer-disabled:opacity-dim-3"
-      >
-        Accept terms and conditions.
-      </label>
-    </div>
-  </ReactLiveBlock>
-)
-const colors = ['primary', 'secondary', 'success', 'alert', 'error', 'info', 'neutral'] as const
-
-export const Colors = () => (
-  <ReactLiveBlock scope={{ Checkbox }}>
-    {colors.map(color => {
+export const Intent: StoryFn = args => (
+  <>
+    {intent.map(color => {
       return (
         <div className="flex items-center gap-sm" key={color}>
-          <Checkbox id={color} intent={color} />
+          <Checkbox id={color} intent={color} {...args} />
           <label htmlFor={color}>{color}</label>
         </div>
       )
     })}
-  </ReactLiveBlock>
+  </>
 )
