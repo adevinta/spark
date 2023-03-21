d001449c1937d6b31802ab75c9e80c861ec6d09e
andresin87 <andresin87@gmail.com>
2023-03-21 10:10:26 +0100

    test(button): add test

--- packages/components/button/src/Button.test.tsx
+++ packages/components/button/src/Button.test.tsx
@@ -6,22 +6,42 @@ import { Button } from './Button'
 
 describe('Button', () => {
   it('should render', () => {
+    // Given
+
+    // When
     render(<Button>Hello World!</Button>)
 
+    // Then
     expect(screen.getByText('Hello World!')).toBeInTheDocument()
   })
 
   it('should trigger click event', async () => {
+    // Given
     const user = userEvent.setup()
     const clickEvent = vi.fn()
 
-    // Given
-    render(<div onClick={clickEvent}>Hello World!</div>)
-
     // When
+    render(<Button onClick={clickEvent}>Hello World!</Button>)
     await user.click(screen.getByText('Hello World!'))
 
     // Then
     expect(clickEvent).toHaveBeenCalledTimes(1)
+  })
+
+  it('should not trigger events', async () => {
+    // Given
+    const user = userEvent.setup()
+    const clickEvent = vi.fn()
+
+    // When
+    render(
+      <Button onClick={clickEvent} disabled>
+        Hello World!
+      </Button>
+    )
+    await user.click(screen.getByText('Hello World!'))
+
+    // Then
+    expect(clickEvent).toHaveBeenCalledTimes(0)
   })
 })
