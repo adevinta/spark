9392b58a4943bb0f410e51e0d3632b9026b8209a
Andrés Alvarez Sordo <a.alvarez.sor@gmail.com>
2023-03-24 11:36:37 +0100

    test(slot): update tests to follow guidelines

--- packages/components/slot/src/Slot.test.tsx
+++ packages/components/slot/src/Slot.test.tsx
@@ -5,26 +5,33 @@ import { Slot } from './Slot'
 
 describe('Slot', () => {
   it('should render wrapped component', () => {
+    // Given
+    const props = {}
+
+    // When
     render(
-      <Slot>
-        <button>Link</button>
-      </Slot>
-    )
-
-    expect(screen.getByRole('button', { name: 'Link' })).toBeInTheDocument()
-  })
-
-  it('should pass props to child component', async () => {
-    const onClick = vitest.fn()
-
-    render(
-      <Slot onClick={onClick}>
+      <Slot {...props}>
         <button>Button</button>
       </Slot>
     )
 
+    // Then
+    expect(screen.getByRole('button', { name: 'Button' })).toBeInTheDocument()
+  })
+
+  it('should pass props to child component', async () => {
+    // Given
+    const props = { onClick: vitest.fn() }
+
+    // When
+    render(
+      <Slot {...props}>
+        <button>Button</button>
+      </Slot>
+    )
     fireEvent.click(screen.getByRole('button', { name: 'Button' }))
 
-    await waitFor(() => expect(onClick).toBeCalled())
+    // Then
+    await waitFor(() => expect(props.onClick).toBeCalled())
   })
 })
