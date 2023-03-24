bbd89c71e3888fba92a80bf472e41533b0649c07
andresin87 <andresin87@gmail.com>
2023-03-24 13:05:26 +0100

    refactor(icons): minor changes

--- packages/components/icons/scripts/utils/indexify.mjs
+++ packages/components/icons/scripts/utils/indexify.mjs
@@ -1,6 +1,6 @@
 const indexify = componentsMap =>
   `${Array.from(componentsMap.entries())
     .map(([name, { dir }]) => `export { ${name} } from './icons/${name}'`)
-    .join('\n')}`
+    .join('\n')}\n`
 
 export default indexify
