f5f31ef8f8bcdc20ed52d816113893525a5effed
andresin87 <andresin87@gmail.com>
2023-03-24 13:05:26 +0100

    refactor(icons): minor changes

--- packages/components/icons/scripts/utils/prettify.mjs
+++ packages/components/icons/scripts/utils/prettify.mjs
@@ -1,7 +1,14 @@
 import { format } from 'prettier'
 
 const prettify = svgString => {
-  return format(svgString, { cursorOffset: 2, parser: 'babel' })
+  return format(svgString, {
+    printWidth: 100,
+    semi: false,
+    singleQuote: true,
+    arrowParens: 'avoid',
+    cursorOffset: 2,
+    parser: 'babel',
+  })
 }
 
 export default prettify
