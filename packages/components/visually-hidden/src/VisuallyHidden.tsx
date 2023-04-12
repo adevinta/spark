a209985c0a3506e34c7f57888d8fda09452be0b2
andresin87 <andresin87@gmail.com>
2023-04-12 13:32:32 +0200

    feat(visually-hidden): froze versions

--- packages/components/visually-hidden/src/VisuallyHidden.tsx
+++ packages/components/visually-hidden/src/VisuallyHidden.tsx
@@ -1,7 +1,7 @@
 import { Root as VisuallyHiddenRoot } from '@radix-ui/react-visually-hidden'
-import { forwardRef, PropsWithChildren } from 'react'
+import { forwardRef, HTMLAttributes, PropsWithChildren } from 'react'
 
-export type SlotProps = PropsWithChildren<React.HTMLAttributes<HTMLElement>>
+export type SlotProps = PropsWithChildren<HTMLAttributes<HTMLElement>>
 
 export const VisuallyHidden = forwardRef<HTMLElement, SlotProps>((props, ref) => {
   return <VisuallyHiddenRoot ref={ref} {...props} />
