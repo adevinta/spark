68759145ea8572c52adcae5c3fc628c0717ec85f
Kiko Ruiz Lloret <kikoruizlloret@gmail.com>
2023-04-14 14:22:02 +0200

    fix(icons): fix imports and tests

--- packages/components/icons/src/index.test.tsx
+++ packages/components/icons/src/index.test.tsx
@@ -1,59 +1,62 @@
+/* eslint-disable max-nested-callbacks */
 import { render, screen } from '@testing-library/react'
 import { describe, expect, it } from 'vitest'
 
-import { Check as IconCheck } from './icons/Check'
+import * as Icons from './index'
 
 describe('Icons', () => {
-  describe('Check icon', () => {
-    it('should be an svg', () => {
-      // Given
-      const props = { 'data-testid': 'testId' }
+  Object.entries(Icons).forEach(([name, Icon]) => {
+    describe(`${name}`, () => {
+      it('should be an svg', () => {
+        // Given
+        const props = { 'data-testid': 'testId' }
 
-      // When
-      render(<IconCheck {...props} />)
-      const element = screen.getByTestId(props['data-testid'])
+        // When
+        render(<Icon {...props} />)
+        const element = screen.getByTestId(props['data-testid'])
 
-      // Then
-      expect(element).not.toBeNull()
-    })
+        // Then
+        expect(element).not.toBeNull()
+      })
 
-    it('should not have height and width', () => {
-      // Given
-      const props = { 'data-testid': 'testId' }
+      it('should not have height and width', () => {
+        // Given
+        const props = { 'data-testid': 'testId' }
 
-      // When
-      render(<IconCheck {...props} />)
-      const element = screen.getByTestId(props['data-testid'])
+        // When
+        render(<Icon {...props} />)
+        const element = screen.getByTestId(props['data-testid'])
 
-      // Then
-      expect(element).not.toHaveAttribute('width')
-      expect(element).not.toHaveAttribute('height')
-    })
+        // Then
+        expect(element).not.toHaveAttribute('width')
+        expect(element).not.toHaveAttribute('height')
+      })
 
-    it('should have viewBox="0 0 24 24"', () => {
-      // Given
-      const props = { 'data-testid': 'testId' }
+      it('should have viewBox="0 0 24 24"', () => {
+        // Given
+        const props = { 'data-testid': 'testId' }
 
-      // When
-      render(<IconCheck {...props} />)
-      const element = screen.getByTestId(props['data-testid'])
+        // When
+        render(<Icon {...props} />)
+        const element = screen.getByTestId(props['data-testid'])
 
-      // Then
-      expect(element).toHaveAttribute('viewBox', '0 0 24 24')
-    })
+        // Then
+        expect(element).toHaveAttribute('viewBox', '0 0 24 24')
+      })
 
-    it('should have a title', () => {
-      // Given
-      const props = {
-        title: 'title',
-      }
+      it('should have a title', () => {
+        // Given
+        const props = {
+          title: 'title',
+        }
 
-      // When
-      render(<IconCheck {...props} />)
-      const element = screen.getByTitle(props.title)
+        // When
+        render(<Icon {...props} />)
+        const element = screen.getByTitle(props.title)
 
-      // Then
-      expect(element).not.toBeNull()
+        // Then
+        expect(element).not.toBeNull()
+      })
     })
   })
 })
