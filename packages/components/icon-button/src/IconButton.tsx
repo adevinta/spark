65ab08ddcc379bc8451b17f77747bf32a7ab6367
Andrés Alvarez Sordo <a.alvarez.sor@gmail.com>
2023-04-19 15:31:29 +0200

    feat(icon-button): add icon button component

---
+++ packages/components/icon-button/src/IconButton.tsx
@@ -0,0 +1,36 @@
+import { Button, ButtonProps } from '@spark-ui/button'
+import React from 'react'
+
+import { iconButtonVariants } from './IconButton.variants'
+
+export interface IconButtonProps extends ButtonProps {
+  'aria-label': string
+}
+
+export const IconButton = React.forwardRef<HTMLButtonElement, IconButtonProps>(
+  (
+    {
+      className,
+      design = 'filled',
+      disabled = false,
+      intent = 'primary',
+      shape = 'rounded',
+      size = 'md',
+      ...others
+    },
+    ref
+  ) => {
+    return (
+      <Button
+        ref={ref}
+        className={iconButtonVariants({ size })}
+        design={design}
+        disabled={disabled}
+        intent={intent}
+        shape={shape}
+        size={size}
+        {...others}
+      />
+    )
+  }
+)
