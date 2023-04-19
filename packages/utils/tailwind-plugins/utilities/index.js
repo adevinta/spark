a1dc67bc292447629b9a98170a1b48b914b31b95
acd02 <alexandre.cadiot@adevinta.com>
2023-04-19 17:05:32 +0200

    refactor(icon): use new utility class
    Use new utility class

--- packages/utils/tailwind-plugins/utilities/index.js
+++ packages/utils/tailwind-plugins/utilities/index.js
@@ -3,7 +3,7 @@ const plugin = require('tailwindcss/plugin')
 
 module.exports = plugin.withOptions(options => ({ addUtilities }) => {
   addUtilities({
-    '.current-icon-size': {
+    '.current-font-size': {
       width: '1em',
       height: '1em',
     },
