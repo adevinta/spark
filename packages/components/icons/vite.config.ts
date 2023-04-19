3e72f85dc78e72fa4727df486a3fd69f212aa460
Kiko Ruiz Lloret <kikoruizlloret@gmail.com>
2023-04-19 09:38:28 +0200

    feat(icons): change component build

--- packages/components/icons/vite.config.ts
+++ packages/components/icons/vite.config.ts
@@ -1,6 +1,23 @@
-import path from 'path'
+import path, { resolve } from 'node:path'
+import glob from 'fast-glob'
 import { getComponentConfiguration } from '../../../config/index'
+import { createRequire } from 'module'
 
+const require = createRequire(import.meta.url)
 const { name } = require(path.resolve(__dirname, 'package.json'))
+const baseConfig = getComponentConfiguration(process.cwd(), name)
+const icons = glob.sync(['src/icons/*.tsx'])
 
-export default getComponentConfiguration(process.cwd(), name)
+export default {
+  ...baseConfig,
+  build: {
+    ...baseConfig.build,
+    lib: {
+      entry: [...icons, 'src/index.ts'],
+      fileName: (_, entryName) => {
+        return entryName === 'index' ? `${entryName}.js` : `icons/${entryName}.js`
+      },
+      formats: ['es', 'cjs'],
+    },
+  },
+}
