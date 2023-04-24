92f37c23e89d5b491a8feef9cf8764c0dc4c1b66
acd02 <alexandre.cadiot@adevinta.com>
2023-04-24 13:50:40 +0200

    feat(tailwind-plugins): update transition theme values to match figma
    Update transition theme values to match Figma
    
    #642

--- packages/utils/tailwind-plugins/spark-theme/constants.js
+++ packages/utils/tailwind-plugins/spark-theme/constants.js
@@ -10,6 +10,30 @@ const unassignedColors = {
   transparent: 'transparent',
 }
 
+const transitionDurationLookup = {
+  0: '0ms',
+  50: '50ms',
+  100: '100ms',
+  200: '200ms',
+  300: '300ms',
+  400: '400ms',
+  500: '500ms',
+  600: '600ms',
+  800: '800ms',
+}
+
+const transitionTimingFunctionLookup = {
+  'in-back': 'cubic-bezier(0.3, -0.05, 0.7, -0.5)',
+  'out-back': 'cubic-bezier(0.45, 1.45, 0.8, 1)',
+  'in-out-back': 'cubic-bezier(0.42, 0, 0.58, 1)',
+}
+
 const DEFAULT_KEY = 'DEFAULT'
 
-module.exports = { tailwindCategoryKeys, unassignedColors, DEFAULT_KEY }
+module.exports = {
+  tailwindCategoryKeys,
+  unassignedColors,
+  transitionDurationLookup,
+  transitionTimingFunctionLookup,
+  DEFAULT_KEY,
+}
