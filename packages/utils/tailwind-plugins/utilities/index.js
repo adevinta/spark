289ff71d39e2c064132ed0d8c55e758a1ab21695
acd02 <alexandre.cadiot@adevinta.com>
2023-04-26 11:34:34 +0200

    feat(tailwind-plugins): add new "shadow-border-transition" utility class
    Add new "shadow-border-transition" utility class for handling common transitions across multiple
    components
    
    #676

--- packages/utils/tailwind-plugins/utilities/index.js
+++ packages/utils/tailwind-plugins/utilities/index.js
@@ -1,11 +1,16 @@
 /* eslint-disable-next-line @typescript-eslint/no-var-requires */
 const plugin = require('tailwindcss/plugin')
 
-module.exports = plugin.withOptions(options => ({ addUtilities }) => {
+module.exports = plugin.withOptions(options => ({ addUtilities, theme }) => {
   addUtilities({
     '.current-font-size': {
       width: '1em',
       height: '1em',
+    },
+    '.shadow-border-transition': {
+      transition: `box-shadow ${theme('transitionDuration.100')} ${theme(
+        'transitionTimingFunction.in'
+      )}, border-color ${theme('transitionDuration.300')} ${theme('transitionTimingFunction.in')}`,
     },
   })
 })
