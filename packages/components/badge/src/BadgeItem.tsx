087511607515c13bafeac0affc365395d7e34c64
Kiko Ruiz Lloret <kikoruizlloret@gmail.com>
2023-05-02 16:21:07 +0200

    feat(badge): implement accessibility specs

--- packages/components/badge/src/BadgeItem.tsx
+++ packages/components/badge/src/BadgeItem.tsx
@@ -1,7 +1,20 @@
 import { forwardRef, HTMLAttributes, PropsWithRef } from 'react'
 
 import { styles, type StylesProps } from './BadgeItem.styles'
-import { DEFAULT_INTENT, DEFAULT_OVERFLOW_COUNT, DEFAULT_SIZE, DEFAULT_TYPE } from './config'
+import {
+  DEFAULT_INTENT,
+  DEFAULT_LABELS,
+  DEFAULT_OVERFLOW_COUNT,
+  DEFAULT_SIZE,
+  DEFAULT_TYPE,
+} from './config'
+
+interface ACCESSIBILITY_LABELS {
+  noCount?: string
+  singleCount?: string
+  pluralCount?: string
+  moreThanOverflowCount?: string
+}
 
 export interface BadgeItemProps extends PropsWithRef<HTMLAttributes<HTMLSpanElement>>, StylesProps {
   /**
@@ -12,6 +25,10 @@ export interface BadgeItemProps extends PropsWithRef<HTMLAttributes<HTMLSpanElem
    * Maximum numeric value to be dispayed as a count value.
    */
   overflowCount?: number
+  /**
+   * Custom labels for accessibility purposes than can be set for each count case.
+   */
+  labels?: ACCESSIBILITY_LABELS
   /**
    * Describes the way the component is displayed: relative to another element or just standalone.
    */
@@ -26,14 +43,25 @@ export const BadgeItem = forwardRef<HTMLSpanElement, BadgeItemProps>(
       type = DEFAULT_TYPE,
       count,
       overflowCount = DEFAULT_OVERFLOW_COUNT,
+      labels = DEFAULT_LABELS,
       className,
       ...others
     },
     ref
   ) => {
+    const hasOverflow = count && count > overflowCount
+    const regularCountLabel =
+      count && count > 1 ? labels.pluralCount.replace('%COUNT%', count) : labels.singleCount
+    const countLabel = hasOverflow
+      ? labels.moreThanOverflowCount.replace('%OVERFLOW_COUNT%', overflowCount)
+      : regularCountLabel
+    const label = count ? countLabel : labels.noCount
+
     return (
       <span
         ref={ref}
+        data-spark-component="badge"
+        aria-label={label}
         className={styles({
           intent,
           size,
@@ -42,7 +70,7 @@ export const BadgeItem = forwardRef<HTMLSpanElement, BadgeItemProps>(
         })}
         {...others}
       >
-        {count && count > overflowCount ? `${overflowCount}+` : count}
+        {hasOverflow ? `${overflowCount}+` : count}
       </span>
     )
   }
