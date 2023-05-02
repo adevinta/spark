24034d82898f717cd982d474632c29923d2e8b08
Kiko Ruiz Lloret <kikoruizlloret@gmail.com>
2023-05-02 16:22:03 +0200

    test(badge): add test cases

--- packages/components/badge/src/Badge.test.tsx
+++ packages/components/badge/src/Badge.test.tsx
@@ -1,12 +1,53 @@
-import { render, screen } from '@testing-library/react'
+import { getByLabelText, render, screen } from '@testing-library/react'
 import { describe, expect, it } from 'vitest'
 
 import { Badge } from './Badge'
 
-describe('Badge', () => {
+describe.only('Badge', () => {
   it('should render', () => {
+    render(<Badge />)
+
+    expect(document.querySelector('[data-spark-component="badge"]')).toBeInTheDocument()
+  })
+
+  it('should be empty by default', () => {
+    render(<Badge />)
+
+    expect(document.querySelector('[data-spark-component="badge"]')).toBeEmptyDOMElement()
+  })
+
+  it('should has DOM content when passing a children', () => {
     render(<Badge>Hello World!</Badge>)
 
-    expect(screen.getByText('Hello World!')).toBeInTheDocument()
+    const helloWorldContent = (content, element) => {
+      return element.tagName.toLowerCase() === 'div' && content.startsWith('Hello World!')
+    }
+    const childrenWithText = screen.getByText(helloWorldContent)
+
+    expect(childrenWithText).toBeInTheDocument()
+  })
+
+  it('should print properly the label when there is no count', () => {
+    render(<Badge />)
+
+    expect(screen.getByLabelText('New notification')).toBeInTheDocument()
+  })
+
+  it('should print properly the label when count is 1', () => {
+    render(<Badge count={1} />)
+
+    expect(screen.getByLabelText('1 notification')).toBeInTheDocument()
+  })
+
+  it('should print properly the label when count is 99', () => {
+    render(<Badge count={99} />)
+
+    expect(screen.getByLabelText('99 notifications')).toBeInTheDocument()
+  })
+
+  it('should print properly the label when count is 100', () => {
+    render(<Badge count={100} />)
+
+    expect(screen.getByLabelText('More than 99 notifications')).toBeInTheDocument()
   })
 })
