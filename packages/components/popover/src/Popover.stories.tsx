5ba35f50133dddae5be1021086d8a175ec3b4436
Powerplex <julien.bonnin.dev@gmail.com>
2023-05-03 15:55:50 +0200

    feat(popover): popover controlled mode example

--- packages/components/popover/src/Popover.stories.tsx
+++ packages/components/popover/src/Popover.stories.tsx
@@ -16,13 +16,34 @@ export default meta
 export const Default: StoryFn = _args => {
   return (
     <div className="h-sz-240 bg-primary-container border-md border-primary flex items-center justify-center rounded-sm border-dashed">
-      <Popover open>
+      <Popover>
         <Popover.Trigger asChild>
           <Button>Trigger popover</Button>
         </Popover.Trigger>
         <Popover.Content sideOffset={5}>some text</Popover.Content>
       </Popover>
     </div>
+  )
+}
+
+export const Controlled: StoryFn = () => {
+  const [open, setOpen] = useState(false)
+
+  return (
+    <>
+      <div className="gap-lg mb-lg flex">
+        <Button onClick={() => setOpen(true)}>Open popover</Button>
+        <Button onClick={() => setOpen(false)}>Close popover</Button>
+      </div>
+      <div className="h-sz-240 bg-primary-container border-md border-primary flex items-center justify-center rounded-sm border-dashed">
+        <Popover open={open}>
+          <Popover.Anchor asChild>
+            <p>Popover is attached to this text (anchor)</p>
+          </Popover.Anchor>
+          <Popover.Content onInteractOutside={() => setOpen(false)}>some text</Popover.Content>
+        </Popover>
+      </div>
+    </>
   )
 }
 
@@ -44,7 +65,9 @@ export const Positionning: StoryFn = _args => {
         <p className="text-headline-2">Side:</p>
         <RadioGroup value={currentSide} onValueChange={handleChangeSide} orientation="horizontal">
           {['bottom', 'top', 'left', 'right'].map(side => (
-            <Radio value={side}>{side}</Radio>
+            <Radio key={side} value={side}>
+              {side}
+            </Radio>
           ))}
         </RadioGroup>
       </div>
@@ -94,6 +117,7 @@ export const Positionning: StoryFn = _args => {
  *
  * TODO LIST:
  * - Implement Popover.Close
+ * - Implement Popover.Anchor
  * - Implement Popover.Arrow
  * - Implement Popover.Overlay (not in radix ? v2 ?)
  * - couple Popover.Content with Portal (implement `forceMount`)
--- packages/components/popover/src/Popover.tsx
+++ packages/components/popover/src/Popover.tsx
@@ -1,16 +1,20 @@
 import type { FC } from 'react'
 
+import { Anchor, type AnchorProps } from './PopoverAnchor'
 import { Content, type ContentProps } from './PopoverContent'
 import { Root, type RootProps } from './PopoverRoot'
 import { Trigger, type TriggerProps } from './PopoverTrigger'
 
 Trigger.displayName = 'Popover.Trigger'
+Anchor.displayName = 'Popover.Anchor'
 Content.displayName = 'Popover.Content'
 
 export const Popover: FC<RootProps> & {
   Trigger: FC<TriggerProps>
+  Anchor: FC<AnchorProps>
   Content: FC<ContentProps>
 } = Object.assign(Root, {
   Trigger,
+  Anchor,
   Content,
 })
