d06fe51b253d57aa6e88d19d31879cf9ca418a83
Andrés Alvarez Sordo <a.alvarez.sor@gmail.com>
2023-05-11 10:28:53 +0200

    test: mock resize observer

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,1 +1,9 @@
 import '@testing-library/jest-dom'
+
+import { vitest } from 'vitest'
+
+global.ResizeObserver = vitest.fn().mockImplementation(() => ({
+  observe: vitest.fn(),
+  unobserve: vitest.fn(),
+  disconnect: vitest.fn(),
+}))
