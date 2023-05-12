c2e6d7b800317a23b474023ddffd69d61830f72c
Andrés Alvarez Sordo <a.alvarez.sor@gmail.com>
2023-05-12 08:58:07 +0200

    feat(slot): add slottable component

--- packages/components/slot/src/Slot.tsx
+++ packages/components/slot/src/Slot.tsx
@@ -1,6 +1,8 @@
 import { Slot as SlotPrimitive } from '@radix-ui/react-slot'
 import { forwardRef, PropsWithChildren } from 'react'
 
+export { Slottable } from '@radix-ui/react-slot'
+
 export type SlotProps = PropsWithChildren<React.HTMLAttributes<HTMLElement>>
 
 export const Slot = forwardRef<HTMLElement, SlotProps>((props, ref) => {
