4b977bd3b54470429ad1298860a1702568a2cdaf
Powerplex <julien.bonnin.dev@gmail.com>
2023-05-26 17:18:04 +0200

    docs: disabled actions addon

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -25,7 +25,14 @@ module.exports = {
   ],
   addons: [
     '@storybook/addon-links',
-    '@storybook/addon-essentials',
+    // '@storybook/addon-essentials',
+    {
+      name: '@storybook/addon-essentials',
+      options: {
+        actions: false,
+        controls: false,
+      },
+    },
     '@storybook/addon-interactions',
     '@storybook/addon-styling',
     '@storybook/addon-mdx-gfm',
