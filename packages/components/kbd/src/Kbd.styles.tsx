9828d805e5cdc6fe05aeab3b01ef08e5c0a32106
andresin87 <andresin87@gmail.com>
2023-06-09 11:01:40 +0200

    feat(kbd): minor styling fixes

--- packages/components/kbd/src/Kbd.styles.tsx
+++ packages/components/kbd/src/Kbd.styles.tsx
@@ -1,5 +1,5 @@
 import { cva } from 'class-variance-authority'
 
 export const kbdStyles = cva([
-  'border-sm border-b-md bg-neutral-container ps-md pe-md border-outline whitespace-nowrap rounded-sm font-mono text-[0.8em] font-bold leading-none',
+  'border-sm border-b-md bg-neutral-container ps-md pe-md border-outline text-caption whitespace-nowrap rounded-sm font-mono font-bold leading-4',
 ])
--- packages/components/kbd/src/Kbd.test.tsx
+++ packages/components/kbd/src/Kbd.test.tsx
@@ -1,5 +1,4 @@
 import { render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
 import { describe, expect, it, vi } from 'vitest'
 
 import { Kbd } from './Kbd'
@@ -9,19 +8,5 @@ describe('Kbd', () => {
     render(<Kbd>Hello World!</Kbd>)
 
     expect(screen.getByText('Hello World!')).toBeInTheDocument()
-  })
-
-  it('should trigger click event', async () => {
-    const user = userEvent.setup()
-    const clickEvent = vi.fn()
-
-    // Given
-    render(<div onClick={clickEvent}>Hello World!</div>)
-
-    // When
-    await user.click(screen.getByText('Hello World!'))
-
-    // Then
-    expect(clickEvent).toHaveBeenCalledTimes(1)
   })
 })
