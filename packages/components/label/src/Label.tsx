62021f8128a0376fa2239f5adf76460392a7fbd0
Andrés Alvarez Sordo <a.alvarez.sor@gmail.com>
2023-06-12 17:49:47 +0200

    feat(label): add required indicator component

--- packages/components/label/src/Label.tsx
+++ packages/components/label/src/Label.tsx
@@ -1,18 +1,16 @@
-import { Label as LabelPrimitive } from '@radix-ui/react-label'
+import { Label as LabelPrimitive, LabelProps as LabelPrimitiveProps } from '@radix-ui/react-label'
 import { cx } from 'class-variance-authority'
-import { ComponentPropsWithoutRef, forwardRef } from 'react'
+import { forwardRef } from 'react'
 
-export interface LabelProps extends ComponentPropsWithoutRef<'label'> {
-  /**
-   * Change the component to the HTML tag or custom component of the only child.
-   */
-  asChild?: boolean
-  /**
-   * The id of the element the label is associated with.
-   */
-  htmlFor?: string
-}
+export type LabelProps = LabelPrimitiveProps
 
 export const Label = forwardRef<HTMLLabelElement, LabelProps>(({ className, ...others }, ref) => {
-  return <LabelPrimitive ref={ref} className={cx('text-body-1', className)} {...others} />
+  return (
+    <LabelPrimitive
+      ref={ref}
+      data-spark-component="label"
+      className={cx('text-body-1', className)}
+      {...others}
+    />
+  )
 })
--- packages/components/label/src/index.ts
+++ packages/components/label/src/index.ts
@@ -1,1 +1,11 @@
-export * from './Label'
+import { Label as Root } from './Label'
+import { LabelRequiredIndicator } from './LabelRequiredIndicator'
+
+export type { LabelProps } from './Label'
+export type { LabelRequiredIndicatorProps } from './LabelRequiredIndicator'
+
+export const Label: typeof Root & {
+  RequiredIndicator: typeof LabelRequiredIndicator
+} = Object.assign(Root, {
+  RequiredIndicator: LabelRequiredIndicator,
+})
