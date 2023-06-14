a1f1605e1f464c01a5ebcdfd1063e98faeaa27ec
Kiko Ruiz Lloret <kikoruizlloret@gmail.com>
2023-06-14 15:47:36 +0200

    feat(input): add improvements and some fixes

--- packages/components/input/src/InputAddon.styles.ts
+++ packages/components/input/src/InputAddon.styles.ts
@@ -1,43 +1,45 @@
 import { cva, VariantProps } from 'class-variance-authority'
 
-export const inputAddonStyles = cva(
-  ['border-sm', 'flex', 'items-center', 'rounded-lg', 'text-body-1'],
-  {
-    variants: {
-      intent: {
-        neutral: ['border-outline'],
-        success: ['border-success'],
-        alert: ['border-alert'],
-        error: ['border-error'],
-      },
-      isDisabled: {
-        true: ['bg-on-surface/dim-5', 'text-on-surface/dim-3', 'cursor-not-allowed'],
-        false: ['bg-surface', 'text-on-surface'],
-      },
-      isFocused: {
-        true: [],
-        false: [],
-      },
-      isHovered: {
-        true: [],
-        false: [],
-      },
+export const inputAddonStyles = cva(['border-sm', 'flex', 'items-center', 'rounded-lg'], {
+  variants: {
+    intent: {
+      neutral: ['border-outline'],
+      success: ['border-success'],
+      alert: ['border-alert'],
+      error: ['border-error'],
     },
-    compoundVariants: [
-      {
-        intent: 'neutral',
-        isHovered: true,
-        isDisabled: false,
-        class: 'border-outline-high',
-      },
-      {
-        intent: 'neutral',
-        isFocused: true,
-        isDisabled: false,
-        class: 'border-outline-high',
-      },
-    ],
-  }
-)
+    isDisabled: {
+      true: ['bg-on-surface/dim-5', 'text-on-surface/dim-3', 'cursor-not-allowed'],
+      false: ['bg-surface', 'text-on-surface'],
+    },
+    isFocused: {
+      true: [],
+      false: [],
+    },
+    isHovered: {
+      true: [],
+      false: [],
+    },
+  },
+  compoundVariants: [
+    {
+      intent: 'neutral',
+      isDisabled: true,
+      class: 'border-on-surface/dim-3',
+    },
+    {
+      intent: 'neutral',
+      isHovered: true,
+      isDisabled: false,
+      class: 'border-outline-high',
+    },
+    {
+      intent: 'neutral',
+      isFocused: true,
+      isDisabled: false,
+      class: 'border-outline-high',
+    },
+  ],
+})
 
 export type InputAddonStylesProps = VariantProps<typeof inputAddonStyles>
