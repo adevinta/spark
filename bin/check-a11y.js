da77545dbf82997a5b3063d498cca7cb6b92a10f
jean-paul-lescouzeres <jean-paul.lescouzeres@adevinta.com>
2023-06-15 10:03:23 +0200

    ci: improves check results log

--- bin/check-a11y.js
+++ bin/check-a11y.js
@@ -7,86 +7,90 @@ import AxeBuilder from '@axe-core/webdriverjs'
 import { Builder, By, until } from 'selenium-webdriver'
 import { Options } from 'selenium-webdriver/firefox.js'
 
-(async () => { 
-  const impactColor = {
-    minor: chalk.hex('#ffff00'),
-    moderate: chalk.hex('#ffa500'),
-    serious: chalk.hex('#ff0000'),
-    critical: chalk.red,
+const impactColor = {
+  minor: chalk.hex('#ffff00'),
+  moderate: chalk.hex('#ffa500'),
+  serious: chalk.hex('#ff0000'),
+  critical: chalk.red,
+}
+
+const { stories } = JSON.parse(readFileSync('dist/stories.json', 'utf8'))
+
+const storiesList = Object.keys(stories).reduce((acc, curr) => {
+  if ((stories[curr].importPath).startsWith('./packages/components/') && !(stories[curr].id).endsWith('--docs')) {
+    acc.push(stories[curr].id)
   }
 
+  return acc
+}, [])
+
+let issues = {
+  minor: 0,
+  moderate: 0,
+  serious: 0,
+  critical: 0,
+}
+
+const checkA11y = async () => { 
   const driver = new Builder()
     .forBrowser('firefox')
     .setFirefoxOptions(new Options().headless())
     .build()
 
-  const { stories } = JSON.parse(readFileSync('dist/stories.json', 'utf8'))
+  for (const storyId of storiesList) {
+    console.log(`Checking accessibility for ${stories[storyId].title}...`)
 
-  const componentsList = Object.keys(stories).reduce((acc, curr) => {
-    if ((stories[curr].importPath).startsWith('./packages/components/') && (stories[curr].id).endsWith('--docs')) {
-      acc.push(stories[curr].id)
-    }
-
-    return acc
-  }, [])
-
-  let hasError = false
-
-  for (const storyId of componentsList) {
     try {
-      await driver.get(`http://localhost:6006/iframe.html?viewMode=docs&id=${storyId}`)
-      await driver.wait(until.elementLocated(By.css("#import")), 10000)
-
+      await driver.get(`http://localhost:6006/iframe.html?viewMode=story&id=${storyId}`)
+  
       const results = await new AxeBuilder(driver)
         .options({
           resultTypes: ['violations', 'incomplete'],
           rules: {
-            // 'landmark-one-main': { enabled: false },
-            // 'region': { enabled: false },
-            // 'color-contrast': { enabled: false },
+            'page-has-heading-one': { enabled: false },
+            'landmark-one-main': { enabled: false },
+            'region': { enabled: false },
+            'color-contrast': { enabled: false },
           },
           runOnly: ['best-practice', 'wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa'],
           reporter: 'no-passes',
         })
         .exclude('.docblock-argstable')
         .analyze()
-
+  
       if (results.violations.length) {
-        hasError = true
-
-        const totalIssues = results.violations.reduce((acc, cur) =>
-          acc += [...cur.nodes].length,
-          0
-        )
-
-        console.log(results.violations)
-        const issues = results.violations.reduce((acc, cur) =>
-          acc[cur.impact] = cur.nodes.length,
-          {
-            minor: 0,
-            moderate: 0,
-            serious: 0,
-            critical: 0,
-          }
-        )
-        console.log(issues)
-        
-        console.log(chalk.bold(stories[storyId].title + ' issues: ' + chalk.red(totalIssues + ' issues')))
+        issues = results.violations.reduce((acc, cur) => {
+          acc[cur.impact] += cur.nodes.length
+          return acc
+        },
+        {
+          minor: 0,
+          moderate: 0,
+          serious: 0,
+          critical: 0,
+        })
+          
+        console.log(chalk.bold(`${stories[storyId].title} issues (${Object.values(issues).reduce((a, b) => a + b, 0)}): ${impactColor.minor(issues.minor)} minor, ${impactColor.moderate(issues.moderate)} moderate, ${impactColor.serious(issues.serious)} serious, ${impactColor.critical(issues.critical)} critical`))
         
         results.violations.forEach((issue) => {
-          const color = issue.impact
-          console.log('- ' + issue.help + impactColor[issue.impact](' (' + issue.nodes.length + ')'))
+          console.log(`- ${issue.help} (${impactColor[issue.impact](`${issue.nodes.length}`)})`)
         })
-
+  
         console.log()
       }
-    
     } catch (e) {
       console.error(chalk.bold.red(e.message))
-      process.exit(1)
     }
   }
 
-  if (hasError) process.exit(1)
+  const totalIssues = Object.values(issues).reduce((a, b) => a + b, 0)
+  if (totalIssues === 0) {
+    console.log()
+    console.log(chalk.bold.green('🎉 Congratulations, no accessibility issue has been found!'))
+  }
+  if (issues.critical) process.exit(1)
+
   await driver.quit()
-})()
+}
+
+checkA11y()
