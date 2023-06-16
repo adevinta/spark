19f9df183dcf40f1ba7df89db9fc1752dff70463
Andrés Alvarez Sordo <a.alvarez.sor@gmail.com>
2023-06-16 13:46:20 +0200

    feat(input): use content prop

--- packages/components/input/src/InputElement.styles.ts
+++ packages/components/input/src/InputElement.styles.ts
@@ -1,7 +1,7 @@
 import { cva, VariantProps } from 'class-variance-authority'
 
 export const inputElementStyles = cva([
-  'text-neutral peer-focus:text-outline-high pointer-events-none absolute top-1/2 -translate-y-1/2',
+  'pointer-events-none absolute top-1/2 -translate-y-1/2 text-neutral peer-focus:text-outline-high',
 ])
 
 export type InputElementStylesProps = VariantProps<typeof inputElementStyles>
