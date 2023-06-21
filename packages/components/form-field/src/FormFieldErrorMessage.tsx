eb15c094094fdbc1b4c18233fe705f5f49186496
Andrés Alvarez Sordo <a.alvarez.sor@gmail.com>
2023-06-21 09:34:48 +0200

    feat(form-field): add form field component states

---
+++ packages/components/form-field/src/FormFieldErrorMessage.tsx
@@ -0,0 +1,20 @@
+import { forwardRef } from 'react'
+
+import { FormFieldStateMessage, FormFieldStateMessageProps } from './FormFieldStateMessage'
+
+export type FormFieldErrorMessageProps = Omit<FormFieldStateMessageProps, 'state'>
+
+export const FormFieldErrorMessage = forwardRef<HTMLSpanElement, FormFieldErrorMessageProps>(
+  (props, ref) => {
+    return (
+      <FormFieldStateMessage
+        ref={ref}
+        data-spark-component="form-field-error-message"
+        state="error"
+        {...props}
+      />
+    )
+  }
+)
+
+FormFieldErrorMessage.displayName = 'FormField.ErrorMessage'
--- packages/components/form-field/src/index.ts
+++ packages/components/form-field/src/index.ts
@@ -1,7 +1,9 @@
 import { FC } from 'react'
 
 import { FormField as Root, FormFieldProps } from './FormField'
+import { FormFieldAlertMessage, FormFieldAlertMessageProps } from './FormFieldAlertMessage'
 import { FormFieldControl, FormFieldControlProps } from './FormFieldControl'
+import { FormFieldErrorMessage, FormFieldErrorMessageProps } from './FormFieldErrorMessage'
 import { FormFieldHelperMessage, FormFieldHelperMessageProps } from './FormFieldHelperMessage'
 import { FormFieldLabel, FormFieldLabelProps } from './FormFieldLabel'
 import {
@@ -9,6 +11,7 @@ import {
   FormFieldRequiredIndicatorProps,
 } from './FormFieldRequiredIndicator'
 import { FormFieldStateMessage, FormFieldStateMessageProps } from './FormFieldStateMessage'
+import { FormFieldSuccessMessage, FormFieldSuccessMessageProps } from './FormFieldSuccessMessage'
 
 export { type FormFieldProps } from './FormField'
 export { type FormFieldStateMessageProps } from './FormFieldStateMessage'
@@ -21,12 +24,18 @@ export const FormField: FC<FormFieldProps> & {
   Label: FC<FormFieldLabelProps>
   Control: FC<FormFieldControlProps>
   StateMessage: FC<FormFieldStateMessageProps>
+  SuccessMessage: FC<FormFieldSuccessMessageProps>
+  AlertMessage: FC<FormFieldAlertMessageProps>
+  ErrorMessage: FC<FormFieldErrorMessageProps>
   HelperMessage: FC<FormFieldHelperMessageProps>
   RequiredIndicator: FC<FormFieldRequiredIndicatorProps>
 } = Object.assign(Root, {
   Label: FormFieldLabel,
   Control: FormFieldControl,
   StateMessage: FormFieldStateMessage,
+  SuccessMessage: FormFieldSuccessMessage,
+  AlertMessage: FormFieldAlertMessage,
+  ErrorMessage: FormFieldErrorMessage,
   HelperMessage: FormFieldHelperMessage,
   RequiredIndicator: FormFieldRequiredIndicator,
 })
