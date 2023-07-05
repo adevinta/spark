37ee17a0ddd530c3b1aba6769c72b0cc51d6ecb6
andresin87 <andresin87@gmail.com>
2023-07-05 18:28:11 +0200

    feat(chip): fix content details

--- packages/components/chip/src/index.ts
+++ packages/components/chip/src/index.ts
@@ -2,10 +2,13 @@ import { FC } from 'react'
 
 import { Chip as Root, ChipProps } from './Chip'
 import { ChipClearButton } from './ChipClearButton'
+import { ChipContent } from './ChipContent'
 
 export { type ChipClearButtonProps } from './ChipClearButton'
+export { type ChipContentProps } from './ChipContent'
 
 ChipClearButton.displayName = 'Chip.ClearButton'
 export const Chip: FC<ChipProps> & {
   ClearButton: typeof ChipClearButton
-} = Object.assign(Root, { ClearButton: ChipClearButton })
+  Content: typeof ChipContent
+} = Object.assign(Root, { ClearButton: ChipClearButton, Content: ChipContent })
