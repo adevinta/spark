bf00c878406df0297b44cac3c5df958a449d99e7
andresin87 <andresin87@gmail.com>
2023-07-07 10:01:07 +0200

    feat(chip): remove unnecesary file

--- packages/components/chip/src/ChipContent.tsx
+++ packages/components/chip/src/ChipContent.tsx
@@ -1,13 +1,9 @@
 import React, { ComponentPropsWithoutRef, forwardRef } from 'react'
 
-import { type ChipContentStylesProps } from './ChipContent.styles'
-
-export interface ChipContentProps
-  extends ComponentPropsWithoutRef<'span'>,
-    ChipContentStylesProps {}
-
-export const ChipContent = forwardRef<HTMLSpanElement, ChipContentProps>(({ children }) => {
-  return <span className="inline-block grow truncate">{children}</span>
-})
+export const ChipContent = forwardRef<HTMLSpanElement, ComponentPropsWithoutRef<'span'>>(
+  ({ children }) => {
+    return <span className="inline-block grow truncate">{children}</span>
+  }
+)
 
 ChipContent.displayName = 'Chip.Content'
