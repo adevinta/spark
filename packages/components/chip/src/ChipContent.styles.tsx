f3df5490ee5e3be6839519a320edb2b5673d7e1d
andresin87 <andresin87@gmail.com>
2023-07-07 10:01:07 +0200

    feat(chip): remove unnecesary file

--- packages/components/chip/src/ChipContent.styles.tsx
+++
@@ -1,5 +0,0 @@
-import { cva, type VariantProps } from 'class-variance-authority'
-
-export const chipContentStyles = cva(['inline-block grow truncate'])
-
-export type ChipContentStylesProps = VariantProps<typeof chipContentStyles>
