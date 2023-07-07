cc01d513a4dae43641e086aec06188562c6da7b4
andresin87 <andresin87@gmail.com>
2023-07-07 11:08:24 +0200

    feat(chip): leading icon

--- packages/components/chip/src/index.ts
+++ packages/components/chip/src/index.ts
@@ -3,12 +3,19 @@ import { FC } from 'react'
 import { Chip as Root, ChipProps } from './Chip'
 import { ChipClearButton } from './ChipClearButton'
 import { ChipContent } from './ChipContent'
+import { ChipLeadingIcon } from './ChipLeadingIcon'
 
 export { type ChipClearButtonProps } from './ChipClearButton'
 export { type ChipContentProps } from './ChipContent'
+export { type ChipLeadingIconProps } from './ChipLeadingIcon'
 
 ChipClearButton.displayName = 'Chip.ClearButton'
 export const Chip: FC<ChipProps> & {
   ClearButton: typeof ChipClearButton
   Content: typeof ChipContent
-} = Object.assign(Root, { ClearButton: ChipClearButton, Content: ChipContent })
+  LeadingIcon: typeof ChipLeadingIcon
+} = Object.assign(Root, {
+  ClearButton: ChipClearButton,
+  Content: ChipContent,
+  LeadingIcon: ChipLeadingIcon,
+})
