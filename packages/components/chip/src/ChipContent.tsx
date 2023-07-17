dc796e675f392be359a92b45667992a373086eac
andresin87 <andresin87@gmail.com>
2023-07-17 11:10:56 +0200

    refactor(chip): add classnames aplliing PR suggestions

--- packages/components/chip/src/ChipContent.tsx
+++ packages/components/chip/src/ChipContent.tsx
@@ -2,8 +2,14 @@ import React, { ComponentPropsWithoutRef, forwardRef } from 'react'
 
 export type ChipContentProps = ComponentPropsWithoutRef<'span'>
 
-export const ChipContent = forwardRef<HTMLSpanElement, ChipContentProps>(({ children }) => {
-  return <span className="inline-block grow truncate">{children}</span>
-})
+export const ChipContent = forwardRef<HTMLSpanElement, ChipContentProps>(
+  ({ children, className }) => {
+    return (
+      <span className={['inline-block grow truncate', className].filter(Boolean).join(' ')}>
+        {children}
+      </span>
+    )
+  }
+)
 
 ChipContent.displayName = 'Chip.Content'
