976845996abcc28c04d18d13ecda0b2e14839e03
andresin87 <andresin87@gmail.com>
2023-07-17 11:10:56 +0200

    refactor(chip): add classnames aplliing PR suggestions

--- packages/components/chip/src/ChipLeadingIcon.tsx
+++ packages/components/chip/src/ChipLeadingIcon.tsx
@@ -2,8 +2,16 @@ import React, { ComponentPropsWithoutRef, forwardRef } from 'react'
 
 export type ChipLeadingIconProps = ComponentPropsWithoutRef<'span'>
 export const ChipLeadingIcon = forwardRef<HTMLSpanElement, ComponentPropsWithoutRef<'span'>>(
-  ({ children }) => {
-    return <span className="mr-sm flex h-full items-center justify-center">{children}</span>
+  ({ children, className }) => {
+    return (
+      <span
+        className={['mr-sm flex h-full items-center justify-center', className]
+          .filter(Boolean)
+          .join(' ')}
+      >
+        {children}
+      </span>
+    )
   }
 )
 
