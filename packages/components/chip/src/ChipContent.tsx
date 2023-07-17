0c4dd4b1b3377e852e10b5f9c3ddb9038b8a517a
andresin87 <andresin87@gmail.com>
2023-07-17 10:07:39 +0200

    refactor(chip): add type

--- packages/components/chip/src/ChipContent.tsx
+++ packages/components/chip/src/ChipContent.tsx
@@ -1,6 +1,7 @@
 import React, { ComponentPropsWithoutRef, forwardRef } from 'react'
 
 export type ChipContentProps = ComponentPropsWithoutRef<'span'>
+
 export const ChipContent = forwardRef<HTMLSpanElement, ChipContentProps>(({ children }) => {
   return <span className="inline-block grow truncate">{children}</span>
 })
