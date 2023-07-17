2a6510274e51f6be46c5955f533b03a2d918deca
andresin87 <andresin87@gmail.com>
2023-07-17 12:11:47 +0200

    refactor(chip): add classnames dependency usage

--- packages/components/chip/src/ChipContent.tsx
+++ packages/components/chip/src/ChipContent.tsx
@@ -1,14 +1,11 @@
+import { cx } from 'class-variance-authority'
 import React, { ComponentPropsWithoutRef, forwardRef } from 'react'
 
 export type ChipContentProps = ComponentPropsWithoutRef<'span'>
 
 export const ChipContent = forwardRef<HTMLSpanElement, ChipContentProps>(
   ({ children, className }) => {
-    return (
-      <span className={['inline-block grow truncate', className].filter(Boolean).join(' ')}>
-        {children}
-      </span>
-    )
+    return <span className={cx('inline-block grow truncate', className)}>{children}</span>
   }
 )
 
