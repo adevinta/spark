32d59283e35d1d1f18f01ea832c171791fb5c6db
Andrés Alvarez Sordo <a.alvarez.sor@gmail.com>
2023-07-18 13:13:09 +0200

    feat(input): change icon button to be custom

--- packages/components/input/src/InputClearButton.tsx
+++ packages/components/input/src/InputClearButton.tsx
@@ -1,26 +1,14 @@
 import { Icon } from '@spark-ui/icon'
-import { IconButton, IconButtonProps } from '@spark-ui/icon-button'
 import { DeleteFill } from '@spark-ui/icons/dist/icons/DeleteFill'
 import { cx } from 'class-variance-authority'
-import { forwardRef, MouseEventHandler } from 'react'
+import { ComponentPropsWithoutRef, forwardRef, MouseEventHandler } from 'react'
 
 import { useInputGroup } from './InputGroupContext'
 
-export type InputClearButtonProps = IconButtonProps
+export type InputClearButtonProps = ComponentPropsWithoutRef<'button'>
 
 export const InputClearButton = forwardRef<HTMLButtonElement, InputClearButtonProps>(
-  (
-    {
-      className,
-      intent = 'neutral',
-      design = 'ghost',
-      size = 'sm',
-      tabIndex = -1,
-      onClick,
-      ...others
-    },
-    ref
-  ) => {
+  ({ className, tabIndex = -1, onClick, ...others }, ref) => {
     const { onClear, hasTrailingIcon } = useInputGroup()
 
     const handleClick: MouseEventHandler<HTMLButtonElement> = event => {
@@ -34,24 +22,23 @@ export const InputClearButton = forwardRef<HTMLButtonElement, InputClearButtonPr
     }
 
     return (
-      <IconButton
+      <button
         ref={ref}
         className={cx(
           className,
           'absolute top-1/2 -translate-y-1/2 pointer-events-auto',
-          hasTrailingIcon ? 'right-3xl' : 'right-sm'
+          'inline-flex items-center justify-center h-full outline-none',
+          'text-neutral hover:text-neutral-hovered',
+          hasTrailingIcon ? 'right-3xl px-[var(--sz-12)]' : 'right-none pr-lg pl-md'
         )}
         tabIndex={tabIndex}
-        intent={intent}
-        design={design}
-        size={size}
         onClick={handleClick}
         {...others}
       >
         <Icon size="sm">
           <DeleteFill />
         </Icon>
-      </IconButton>
+      </button>
     )
   }
 )
