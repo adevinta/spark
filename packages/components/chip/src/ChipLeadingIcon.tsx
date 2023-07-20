1ef118fb4b8c5baeae59b2545e400b40e674405c
andresin87 <andresin87@gmail.com>
2023-07-20 12:24:12 +0200

    feat(chip): minor changes aligning the stages

--- packages/components/chip/src/ChipLeadingIcon.tsx
+++ packages/components/chip/src/ChipLeadingIcon.tsx
@@ -2,10 +2,14 @@ import { cx } from 'class-variance-authority'
 import React, { ComponentPropsWithoutRef, forwardRef } from 'react'
 
 export type ChipLeadingIconProps = ComponentPropsWithoutRef<'span'>
+
 export const ChipLeadingIcon = forwardRef<HTMLSpanElement, ComponentPropsWithoutRef<'span'>>(
-  ({ children, className }) => {
+  ({ children, className }, forwardedRef) => {
     return (
-      <span className={cx('mr-sm flex h-full items-center justify-center', className)}>
+      <span
+        className={cx('mr-sm flex h-full items-center justify-center', className)}
+        ref={forwardedRef}
+      >
         {children}
       </span>
     )
