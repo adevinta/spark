c23c479bfc9accb2027b4114219733596bf724e6
Powerplex <julien.bonnin.dev@gmail.com>
2023-07-27 11:45:54 +0200

    style(dialog): base styles for dialog overlay and content

--- packages/components/dialog/src/DialogContent.tsx
+++ packages/components/dialog/src/DialogContent.tsx
@@ -1,13 +1,26 @@
 import * as RadixDialog from '@radix-ui/react-dialog'
+import { cx } from 'class-variance-authority'
 import { forwardRef, type ReactElement, type Ref } from 'react'
 
 export type ContentProps = RadixDialog.DialogContentProps
 
 export const Content = forwardRef(
-  ({ children, ...rest }: ContentProps, ref: Ref<HTMLDivElement>): ReactElement => (
-    <RadixDialog.Content data-spark-component="dialog-content" ref={ref} {...rest}>
-      {children}
-    </RadixDialog.Content>
+  ({ children, className, ...rest }: ContentProps, ref: Ref<HTMLDivElement>): ReactElement => (
+    <div className="absolute left-none top-none h-screen w-screen">
+      <RadixDialog.Content
+        data-spark-component="dialog-content"
+        ref={ref}
+        className={cx(
+          ['absolute', 'top-1/2', 'left-1/2', '-translate-x-1/2', '-translate-y-1/2', 'z-modal'],
+          ['bg-surface', 'shadow-md', 'rounded-lg'],
+          ['p-xl'],
+          className
+        )}
+        {...rest}
+      >
+        {children}
+      </RadixDialog.Content>
+    </div>
   )
 )
 
--- packages/components/dialog/src/DialogOverlay.tsx
+++ packages/components/dialog/src/DialogOverlay.tsx
@@ -1,11 +1,20 @@
 import * as RadixDialog from '@radix-ui/react-dialog'
+import { cx } from 'class-variance-authority'
 import { forwardRef, type ReactElement, type Ref } from 'react'
 
 export type OverlayProps = RadixDialog.DialogOverlayProps
 
 export const Overlay = forwardRef(
-  ({ children, ...rest }: OverlayProps, ref: Ref<HTMLDivElement>): ReactElement => (
-    <RadixDialog.Overlay ref={ref} {...rest}>
+  ({ children, className, ...rest }: OverlayProps, ref: Ref<HTMLDivElement>): ReactElement => (
+    <RadixDialog.Overlay
+      ref={ref}
+      className={cx(
+        ['absolute', 'top-none', 'left-none', 'w-screen', 'h-screen', 'z-overlay'],
+        ['bg-overlay/dim-3'],
+        className
+      )}
+      {...rest}
+    >
       {children}
     </RadixDialog.Overlay>
   )
