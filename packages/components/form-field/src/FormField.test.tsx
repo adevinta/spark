410db3b63b3e11552dece8e1e1f5f02d9d4993b9
Kiko Ruiz Lloret <kikoruizlloret@gmail.com>
2023-07-28 09:41:15 +0200

    test(form-field): add an extra test

--- packages/components/form-field/src/FormField.test.tsx
+++ packages/components/form-field/src/FormField.test.tsx
@@ -246,4 +246,39 @@ describe('FormField error message', () => {
     expect(inputEl).toHaveValue('Hello')
     expect(charsCountEl).toBeVisible()
   })
+
+  it('should render the characters count component properly when `value` is undefined', () => {
+    const MAX_LENGTH = 6
+    const DEFAULT_VALUE = undefined
+
+    render(
+      <FormField name="text">
+        <FormField.Label>Text</FormField.Label>
+
+        <FormField.Control>
+          {({ id, name }) => (
+            <input
+              type="text"
+              id={id}
+              name={name}
+              defaultValue={DEFAULT_VALUE}
+              maxLength={MAX_LENGTH}
+            />
+          )}
+        </FormField.Control>
+
+        <div className="flex justify-between gap-md">
+          <FormField.HelperMessage>We will never share your email</FormField.HelperMessage>
+
+          <FormField.CharactersCount value={DEFAULT_VALUE} maxLength={MAX_LENGTH} />
+        </div>
+      </FormField>
+    )
+
+    const inputEl = screen.getByLabelText('Text')
+    const charsCountEl = screen.getByText('0/6')
+
+    expect(inputEl).toHaveValue('')
+    expect(charsCountEl).toBeVisible()
+  })
 })
