8062396c3f70799c54fce9ef36cc2d1cbebb996e
Powerplex <julien.bonnin.dev@gmail.com>
2023-08-09 18:29:26 +0200

    docs: adapted custom ArgTypes table for mobile screens

--- documentation/helpers/ArgTypes/index.tsx
+++ documentation/helpers/ArgTypes/index.tsx
@@ -1,6 +1,6 @@
 import { Tabs } from '@spark-ui/tabs'
 import { ArgTypes as StorybookArgTypes } from '@storybook/blocks'
-import { type FC, type ReactNode } from 'react'
+import { type FC, type ReactNode, useEffect, useState } from 'react'
 
 interface Props<T> {
   of: T
@@ -20,15 +20,41 @@ const ComponentDescription = ({ name, children }: { name: string; children: Reac
   )
 }
 
+function useWindowWidth() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth)
+    }
+
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
+  return windowWidth
+}
+
 export const ArgTypes = <T extends FC>({ of, description, subcomponents = null }: Props<T>) => {
+  const windowWidth = useWindowWidth()
+
   if (!subcomponents) return <StorybookArgTypes of={of} />
 
   const { displayName: name = 'Root' } = of // "Root" in case the root component is missing a displayName
   const subComponentsList = Object.entries(subcomponents)
 
+  const tabsOrientation = windowWidth < 640 ? 'horizontal' : 'vertical'
+
   return (
-    <Tabs defaultValue={name} orientation="vertical" className="sb-unstyled mt-xl">
-      <Tabs.List>
+    <Tabs
+      defaultValue={name}
+      orientation={tabsOrientation}
+      className="sb-unstyled mt-xl overflow-hidden"
+    >
+      <Tabs.List className={tabsOrientation === 'horizontal' ? 'mb-md' : ''}>
         <Tabs.Trigger key={name} value={name}>
           {name}
         </Tabs.Trigger>
