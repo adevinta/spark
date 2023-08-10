b0c02081fc265c8ce9aeed2ceb491b75ebb38778
Powerplex <julien.bonnin.dev@gmail.com>
2023-08-10 15:56:07 +0200

    docs(dialog): advanced example of dialog stories

--- packages/components/dialog/src/Dialog.stories.tsx
+++ packages/components/dialog/src/Dialog.stories.tsx
@@ -1,7 +1,9 @@
 import { Button } from '@spark-ui/button'
+import { FormField } from '@spark-ui/form-field'
+import { Input } from '@spark-ui/input'
 import { RadioGroup } from '@spark-ui/radio-group'
 import { Meta, StoryFn } from '@storybook/react'
-import { useState } from 'react'
+import React, { useState } from 'react'
 
 import { Dialog, type DialogContentProps } from '.'
 
@@ -12,7 +14,7 @@ const meta: Meta<typeof Dialog> = {
 
 export default meta
 
-export const Usage: StoryFn = () => {
+export const Default: StoryFn = () => {
   const [open, setOpen] = useState(false)
 
   return (
@@ -61,50 +63,115 @@ export const Usage: StoryFn = () => {
 }
 
 export const Sizes = () => {
-  const [size, setSizes] = useState<ExcludeNull<DialogContentProps>['size']>('md')
+  const [size, setSize] = useState<ExcludeNull<DialogContentProps>['size']>('md')
   const [open, setOpen] = useState(false)
 
   return (
-    <>
-      <Dialog open={open} onOpenChange={setOpen}>
+    <Dialog open={open} onOpenChange={setOpen}>
+      <div className="flex gap-md">
         <Dialog.Trigger asChild>
-          <Button>Edit size</Button>
+          <Button onClick={() => setSize('sm')}>Small</Button>
         </Dialog.Trigger>
 
-        <Dialog.Portal>
-          <Dialog.Overlay />
+        <Dialog.Trigger asChild>
+          <Button onClick={() => setSize('md')}>Medium</Button>
+        </Dialog.Trigger>
 
-          <Dialog.Content size={size}>
+        <Dialog.Trigger asChild>
+          <Button onClick={() => setSize('md')}>Large</Button>
+        </Dialog.Trigger>
+
+        <Dialog.Trigger asChild>
+          <Button onClick={() => setSize('fullscreen')}>Fullscreen</Button>
+        </Dialog.Trigger>
+      </div>
+
+      <Dialog.Portal>
+        <Dialog.Overlay />
+
+        <Dialog.Content size={size}>
+          <Dialog.Header>
+            <Dialog.Title>Edit size</Dialog.Title>
+          </Dialog.Header>
+
+          <Dialog.Body className="flex flex-col gap-lg">
+            <Dialog.Description>Please select a dialog size</Dialog.Description>
+
+            <RadioGroup
+              className="flex gap-md"
+              value={size}
+              onValueChange={value => setSize(value as ExcludeNull<DialogContentProps>['size'])}
+            >
+              <RadioGroup.Radio value="sm">Small</RadioGroup.Radio>
+              <RadioGroup.Radio value="md">Medium</RadioGroup.Radio>
+              <RadioGroup.Radio value="lg">Large</RadioGroup.Radio>
+              <RadioGroup.Radio value="fullscreen">Fullscreen</RadioGroup.Radio>
+            </RadioGroup>
+          </Dialog.Body>
+
+          <Dialog.Footer className="flex justify-end gap-md">
+            <Button intent="neutral" design="outlined" onClick={() => setOpen(false)}>
+              Cancel
+            </Button>
+            <Button>Submit</Button>
+          </Dialog.Footer>
+
+          <Dialog.CloseButton aria-label="Close edit size" />
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog>
+  )
+}
+
+export const HTMLForm = () => {
+  const [open, setOpen] = useState(false)
+  const [isAccountCreated, setIsAccountCreated] = useState(false)
+
+  const handleOpenChange = (open: boolean) => {
+    setOpen(open)
+
+    if (!open) setIsAccountCreated(false)
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    setIsAccountCreated(true)
+  }
+
+  return (
+    <Dialog open={open} onOpenChange={handleOpenChange}>
+      <Dialog.Trigger asChild>
+        <Button>Create account</Button>
+      </Dialog.Trigger>
+
+      <Dialog.Portal>
+        <Dialog.Overlay />
+
+        <Dialog.Content>
+          <form onSubmit={handleSubmit}>
             <Dialog.Header>
-              <Dialog.Title>Edit size</Dialog.Title>
+              <Dialog.Title>Create account</Dialog.Title>
             </Dialog.Header>
 
             <Dialog.Body className="flex flex-col gap-lg">
-              <Dialog.Description>Please select a dialog size</Dialog.Description>
-
-              <RadioGroup
-                className="flex gap-md"
-                value={size}
-                onValueChange={value => setSizes(value as ExcludeNull<DialogContentProps>['size'])}
-              >
-                <RadioGroup.Radio value="sm">Small</RadioGroup.Radio>
-                <RadioGroup.Radio value="md">Medium</RadioGroup.Radio>
-                <RadioGroup.Radio value="lg">Large</RadioGroup.Radio>
-                <RadioGroup.Radio value="fullscreen">Fullscreen</RadioGroup.Radio>
-              </RadioGroup>
+              {!isAccountCreated ? (
+                <FormField name="pseudo" isRequired className="flex-1">
+                  <FormField.Label>Pseudo</FormField.Label>
+                  <Input placeholder="Luke" />
+                </FormField>
+              ) : (
+                <p className="text-success">Welcome to Spark !</p>
+              )}
             </Dialog.Body>
 
             <Dialog.Footer className="flex justify-end gap-md">
-              <Button intent="neutral" design="outlined" onClick={() => setOpen(false)}>
-                Cancel
-              </Button>
-              <Button>Submit</Button>
+              {!isAccountCreated && <Button>Submit</Button>}
             </Dialog.Footer>
+          </form>
 
-            <Dialog.CloseButton aria-label="Close edit size" />
-          </Dialog.Content>
-        </Dialog.Portal>
-      </Dialog>
-    </>
+          <Dialog.CloseButton aria-label="Close edit size" />
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog>
   )
 }
