06908564b6870408f9e03a4ea52a91518c4fcbc5
Andrés Alvarez Sordo <a.alvarez.sor@gmail.com>
2023-08-18 14:21:23 +0200

    feat(progress): add first version of progress component

---
+++ packages/components/progress/src/Progress.tsx
@@ -0,0 +1,29 @@
+import {
+  Progress as ProgressPrimitive,
+  ProgressIndicator as ProgressPrimitiveIndicator,
+  ProgressProps as ProgressPrimitiveProps,
+} from '@radix-ui/react-progress'
+import { ElementRef, forwardRef, PropsWithChildren } from 'react'
+
+export type ProgressProps = ProgressPrimitiveProps
+
+export const Progress = forwardRef<
+  ElementRef<typeof ProgressPrimitive>,
+  PropsWithChildren<ProgressProps>
+>(({ value: valueProp, ...others }, ref) => {
+  const value = valueProp ?? 0
+
+  return (
+    <ProgressPrimitive
+      className="relative h-sz-4 w-full transform-gpu overflow-hidden rounded-sm bg-on-background/dim-4"
+      value={value}
+      ref={ref}
+      {...others}
+    >
+      <ProgressPrimitiveIndicator
+        className="h-full w-full bg-main"
+        style={{ transform: `translateX(-${100 - value}%)` }}
+      />
+    </ProgressPrimitive>
+  )
+})
