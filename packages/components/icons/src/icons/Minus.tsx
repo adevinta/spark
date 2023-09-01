e57708202722fe732eb4d36349d43ef01c846e26
solygambas <51904909+solygambas@users.noreply.github.com>
2023-09-01 13:37:59 +0200

    fix(icons): centering on icons

--- packages/components/icons/src/icons/Minus.tsx
+++ packages/components/icons/src/icons/Minus.tsx
@@ -11,11 +11,12 @@ export const Minus = React.forwardRef(
       ref={ref}
       viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
+      data-title="Minus"
       {...{ ...(title && { 'data-title': title }), fill, stroke, ...props }}
       dangerouslySetInnerHTML={{
         __html:
           (title === undefined ? '' : `<title>${title}</title>`) +
-          '<path fill-rule="evenodd" clip-rule="evenodd" d="M2 11.9999C2 11.379 2.44772 10.8756 3 10.8756H21C21.5523 10.8756 22 11.379 22 11.9999C22 12.6208 21.5523 13.1242 21 13.1242H3C2.44772 13.1242 2 12.6208 2 11.9999Z"/>',
+          '<path fill-rule="evenodd" d="m2,12c0-.62.45-1.12,1-1.12h18c.55,0,1,.5,1,1.12s-.45,1.12-1,1.12H3c-.55,0-1-.5-1-1.12Z"/>',
       }}
     />
   )
