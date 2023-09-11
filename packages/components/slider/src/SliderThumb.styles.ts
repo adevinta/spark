16f6f691ea4c32a91998321648dc8a7c1781d224
soykje <jean-paul.lescouzeres@adevinta.com>
2023-09-11 21:49:59 +0200

    style(slider): adds focus-visible and hover states styles

--- packages/components/slider/src/SliderThumb.styles.ts
+++ packages/components/slider/src/SliderThumb.styles.ts
@@ -1,22 +1,31 @@
 import { cva, type VariantProps } from 'class-variance-authority'
 
-export const thumbVariants = cva(['block h-xl w-xl rounded-full'], {
-  variants: {
-    intent: {
-      main: ['bg-main'],
-      support: ['bg-support'],
-      accent: ['bg-accent'],
-      basic: ['bg-basic'],
-      info: ['bg-info'],
-      neutral: ['bg-neutral'],
-      success: ['bg-success'],
-      alert: ['bg-alert'],
-      error: ['bg-error'],
+export const thumbVariants = cva(
+  [
+    'block h-xl w-xl rounded-full cursor-pointer',
+    'hover:ring-8 u-shadow-border-transition',
+    'outline-none',
+    'focus-visible:ring-8 focus-visible:ring-offset-2 focus-visible:ring-offset-outline-high',
+    'spark-disabled:hover:ring-0 spark-disabled:cursor-not-allowed',
+  ],
+  {
+    variants: {
+      intent: {
+        main: ['bg-main hover:ring-main/dim-5 focus-visible:ring-main/dim-5'],
+        support: ['bg-support hover:ring-support/dim-5 focus-visible:ring-support/dim-5'],
+        accent: ['bg-accent hover:ring-accent/dim-5 focus-visible:ring-accent/dim-5'],
+        basic: ['bg-basic hover:ring-basic/dim-5 focus-visible:ring-basic/dim-5'],
+        info: ['bg-info hover:ring-info/dim-5 focus-visible:ring-info/dim-5'],
+        neutral: ['bg-neutral hover:ring-neutral/dim-5 focus-visible:ring-neutral/dim-5'],
+        success: ['bg-success hover:ring-success/dim-5 focus-visible:ring-success/dim-5'],
+        alert: ['bg-alert hover:ring-alert/dim-5 focus-visible:ring-alert/dim-5'],
+        error: ['bg-error hover:ring-error/dim-5 focus-visible:ring-error/dim-5'],
+      },
     },
-  },
-  defaultVariants: {
-    intent: 'basic',
-  },
-})
+    defaultVariants: {
+      intent: 'basic',
+    },
+  }
+)
 
 export type SliderThumbVariantsProps = VariantProps<typeof thumbVariants>
