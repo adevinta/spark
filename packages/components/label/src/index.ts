f303580c59e81c9338f2b8cfc105437de3f6ddc6
Powerplex <julien.bonnin.dev@gmail.com>
2023-09-11 21:47:48 +0200

    fix(label): harmonize label displayNames

--- packages/components/label/src/index.ts
+++ packages/components/label/src/index.ts
@@ -1,13 +1,11 @@
 import { Label as Root } from './Label'
 import { LabelRequiredIndicator } from './LabelRequiredIndicator'
 
-export type { LabelProps } from './Label'
-export type { LabelRequiredIndicatorProps } from './LabelRequiredIndicator'
-
-LabelRequiredIndicator.displayName = 'Label.RequiredIndicator'
-
 export const Label: typeof Root & {
   RequiredIndicator: typeof LabelRequiredIndicator
 } = Object.assign(Root, {
   RequiredIndicator: LabelRequiredIndicator,
 })
+
+export type { LabelProps } from './Label'
+export type { LabelRequiredIndicatorProps } from './LabelRequiredIndicator'
