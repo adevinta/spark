71d00b87e0b9c05dbce64fae237958e36a15166b
Powerplex <julien.bonnin.dev@gmail.com>
2023-09-11 10:51:22 +0200

    refactor(drawer): harmonize drawer displayNames

--- packages/components/drawer/src/index.ts
+++ packages/components/drawer/src/index.ts
@@ -1,5 +1,6 @@
 import type { FC } from 'react'
 
+import { Drawer as Root, type DrawerProps } from './Drawer'
 import { DrawerBody } from './DrawerBody'
 import { DrawerCloseButton } from './DrawerCloseButton'
 import { DrawerContent } from './DrawerContent'
@@ -8,35 +9,10 @@ import { DrawerFooter } from './DrawerFooter'
 import { DrawerHeader } from './DrawerHeader'
 import { DrawerOverlay } from './DrawerOverlay'
 import { DrawerPortal } from './DrawerPortal'
-import { DrawerRoot, type DrawerRootProps } from './DrawerRoot'
 import { DrawerTitle } from './DrawerTitle'
 import { DrawerTrigger } from './DrawerTrigger'
 
-DrawerTrigger.displayName = 'Drawer.Trigger'
-DrawerPortal.displayName = 'Drawer.Portal'
-DrawerOverlay.displayName = 'Drawer.Overlay'
-DrawerContent.displayName = 'Drawer.Content'
-DrawerHeader.displayName = 'Drawer.Header'
-DrawerBody.displayName = 'Drawer.Body'
-DrawerFooter.displayName = 'Drawer.Footer'
-DrawerCloseButton.displayName = 'Drawer.CloseButton'
-DrawerTitle.displayName = 'Drawer.Title'
-DrawerDescription.displayName = 'Drawer.Description'
-
-export { type DrawerRootProps as DrawerProps } from './DrawerRoot'
-export { type DrawerContentProps } from './DrawerContent'
-export { type DrawerHeaderProps } from './DrawerHeader'
-export { type DrawerBodyProps } from './DrawerBody'
-export { type DrawerFooterProps } from './DrawerFooter'
-export { type DrawerTriggerProps } from './DrawerTrigger'
-export { type DrawerOverlayProps } from './DrawerOverlay'
-export { type DrawerPortalProps } from './DrawerPortal'
-export { type DrawerTitleProps } from './DrawerTitle'
-export { type DrawerDescriptionProps } from './DrawerDescription'
-export { type DrawerCloseProps } from './DrawerClose'
-export { type DrawerCloseButtonProps } from './DrawerCloseButton'
-
-export const Drawer: FC<DrawerRootProps> & {
+export const Drawer: FC<DrawerProps> & {
   Trigger: typeof DrawerTrigger
   Portal: typeof DrawerPortal
   Overlay: typeof DrawerOverlay
@@ -47,7 +23,7 @@ export const Drawer: FC<DrawerRootProps> & {
   CloseButton: typeof DrawerCloseButton
   Title: typeof DrawerTitle
   Description: typeof DrawerDescription
-} = Object.assign(DrawerRoot, {
+} = Object.assign(Root, {
   Trigger: DrawerTrigger,
   Portal: DrawerPortal,
   Overlay: DrawerOverlay,
@@ -59,3 +35,16 @@ export const Drawer: FC<DrawerRootProps> & {
   Title: DrawerTitle,
   Description: DrawerDescription,
 })
+
+export { type DrawerProps } from './Drawer'
+export { type DrawerContentProps } from './DrawerContent'
+export { type DrawerHeaderProps } from './DrawerHeader'
+export { type DrawerBodyProps } from './DrawerBody'
+export { type DrawerFooterProps } from './DrawerFooter'
+export { type DrawerTriggerProps } from './DrawerTrigger'
+export { type DrawerOverlayProps } from './DrawerOverlay'
+export { type DrawerPortalProps } from './DrawerPortal'
+export { type DrawerTitleProps } from './DrawerTitle'
+export { type DrawerDescriptionProps } from './DrawerDescription'
+export { type DrawerCloseProps } from './DrawerClose'
+export { type DrawerCloseButtonProps } from './DrawerCloseButton'
