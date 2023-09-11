a16b80bbd8c61cbfc41aafb2fbdbab9c582898c3
Powerplex <julien.bonnin.dev@gmail.com>
2023-09-11 21:49:41 +0200

    fix(popover): harmonize popover displayNames

--- packages/components/popover/src/index.ts
+++ packages/components/popover/src/index.ts
@@ -1,1 +1,28 @@
-export { Popover } from './Popover'
+import type { FC } from 'react'
+
+import { Popover as Root, type PopoverProps } from './Popover'
+import { Anchor } from './PopoverAnchor'
+import { Arrow } from './PopoverArrow'
+import { CloseButton } from './PopoverCloseButton'
+import { Content } from './PopoverContent'
+import { Header } from './PopoverHeader'
+import { Portal } from './PopoverPortal'
+import { Trigger } from './PopoverTrigger'
+
+export const Popover: FC<PopoverProps> & {
+  Anchor: typeof Anchor
+  Arrow: typeof Arrow
+  CloseButton: typeof CloseButton
+  Content: typeof Content
+  Header: typeof Header
+  Portal: typeof Portal
+  Trigger: typeof Trigger
+} = Object.assign(Root, {
+  Anchor,
+  Arrow,
+  CloseButton,
+  Content,
+  Header,
+  Portal,
+  Trigger,
+})
