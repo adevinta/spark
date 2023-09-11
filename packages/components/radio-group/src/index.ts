1c1aaa9067ec45720dc5561184a54147199029a6
Powerplex <julien.bonnin.dev@gmail.com>
2023-09-11 21:52:06 +0200

    fix(radio-group): harmonize radio displayNames

--- packages/components/radio-group/src/index.ts
+++ packages/components/radio-group/src/index.ts
@@ -3,13 +3,11 @@ import type { FC } from 'react'
 import { Radio } from './Radio'
 import { RadioGroup as Root, type RadioGroupProps } from './RadioGroup'
 
-export { type RadioGroupProps } from './RadioGroup'
-export { type RadioProps } from './Radio'
-
-Radio.displayName = 'RadioGroup.Radio'
-
 export const RadioGroup: FC<RadioGroupProps> & {
   Radio: typeof Radio
 } = Object.assign(Root, {
   Radio,
 })
+
+export { type RadioGroupProps } from './RadioGroup'
+export { type RadioProps } from './Radio'
