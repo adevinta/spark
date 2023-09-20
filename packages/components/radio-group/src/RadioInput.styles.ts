f20bdfc5f0d890ffab520d09de73f6938cc5f2ea
soykje <jean-paul.lescouzeres@adevinta.com>
2023-09-20 16:44:20 +0200

    style: harmonize halo styles between components

--- packages/components/radio-group/src/RadioInput.styles.ts
+++ packages/components/radio-group/src/RadioInput.styles.ts
@@ -19,27 +19,23 @@ export const radioInputVariants = cva(
         'intent',
         ['main', 'support', 'accent', 'basic', 'success', 'alert', 'error', 'info', 'neutral']
       >({
-        main: ['border-outline', 'spark-state-checked:border-main', 'hover:ring-main-container'],
+        main: ['border-outline', 'spark-state-checked:border-main', 'hover:ring-main/dim-4'],
         support: [
           'border-outline',
           'spark-state-checked:border-support',
-          'hover:ring-support-container',
+          'hover:ring-support/dim-4',
         ],
-        accent: [
-          'border-outline',
-          'spark-state-checked:border-accent',
-          'hover:ring-accent-container',
-        ],
-        basic: ['border-outline', 'spark-state-checked:border-basic', 'hover:ring-basic-container'],
+        accent: ['border-outline', 'spark-state-checked:border-accent', 'hover:ring-accent/dim-4'],
+        basic: ['border-outline', 'spark-state-checked:border-basic', 'hover:ring-basic/dim-4'],
         neutral: [
           'border-outline',
           'spark-state-checked:border-neutral',
-          'hover:ring-neutral-container',
+          'hover:ring-neutral/dim-4',
         ],
-        info: ['border-outline', 'spark-state-checked:border-info', 'hover:ring-info-container'],
-        success: ['border-success', 'hover:ring-success-container'],
-        alert: ['border-alert', 'hover:ring-alert-container'],
-        error: ['border-error', 'hover:ring-error-container'],
+        info: ['border-outline', 'spark-state-checked:border-info', 'hover:ring-info/dim-4'],
+        success: ['border-success', 'hover:ring-success/dim-4'],
+        alert: ['border-alert', 'hover:ring-alert/dim-4'],
+        error: ['border-error', 'hover:ring-error/dim-4'],
       }),
     },
     defaultVariants: {
