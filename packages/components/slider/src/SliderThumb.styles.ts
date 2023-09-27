1ad47ed9ea8e408c577f1d7fdcba304e2be691d7
soykje <jean-paul.lescouzeres@adevinta.com>
2023-09-27 16:45:38 +0200

    style(slider): upd slider halo styles

--- packages/components/slider/src/SliderThumb.styles.ts
+++ packages/components/slider/src/SliderThumb.styles.ts
@@ -3,7 +3,7 @@ import { cva, type VariantProps } from 'class-variance-authority'
 export const thumbVariants = cva(
   [
     'block h-sz-24 w-sz-24 rounded-full cursor-pointer',
-    'hover:ring-8 u-shadow-border-transition',
+    'hover:ring-4 u-shadow-border-transition',
     'outline-none',
     'focus-visible:u-ring',
     'spark-disabled:hover:ring-0 spark-disabled:cursor-not-allowed',
@@ -11,15 +11,15 @@ export const thumbVariants = cva(
   {
     variants: {
       intent: {
-        main: ['bg-main hover:ring-main/dim-4'],
-        support: ['bg-support hover:ring-support/dim-4'],
-        accent: ['bg-accent hover:ring-accent/dim-4'],
-        basic: ['bg-basic hover:ring-basic/dim-4'],
-        info: ['bg-info hover:ring-info/dim-4'],
-        neutral: ['bg-neutral hover:ring-neutral/dim-4'],
-        success: ['bg-success hover:ring-success/dim-4'],
-        alert: ['bg-alert hover:ring-alert/dim-4'],
-        error: ['bg-error hover:ring-error/dim-4'],
+        main: ['bg-main hover:ring-main-container'],
+        support: ['bg-support hover:ring-support-container'],
+        accent: ['bg-accent hover:ring-accent-container'],
+        basic: ['bg-basic hover:ring-basic-container'],
+        info: ['bg-info hover:ring-info-container'],
+        neutral: ['bg-neutral hover:ring-neutral-container'],
+        success: ['bg-success hover:ring-success-container'],
+        alert: ['bg-alert hover:ring-alert-container'],
+        error: ['bg-error hover:ring-error-container'],
       },
     },
     defaultVariants: {
