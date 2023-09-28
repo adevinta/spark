a9e50f203dfb4e3ba7c2f9b758c46eb923a222c6
andresin87 <andresin87@gmail.com>
2023-09-28 15:23:00 +0200

    fix(cli-utils): doc

--- packages/utils/cli/README.md
+++ packages/utils/cli/README.md
@@ -1,13 +1,13 @@
 # CLI
 > @spark-ui/cli-utils
 
-![storybook](https://img.shields.io/badge/storybook-black?logo=storybook&link=https%3A%2F%2Fsparkui.vercel.app%2F%3Fpath%3D%2Fdocs%2Futils-cli--docs)
-![documentation](https://img.shields.io/badge/documentation-black?logo=googledocs&link=https%3A%2F%2Fsparkui-adv.vercel.app%2Fdocs%2Futil%2Fcli)
-![issue](https://img.shields.io/badge/report%20a%20bug-black?logo=openbugbounty&logoColor=red&link=https%3A%2F%2Fgithub.com%2Fadevinta%2Fspark%2Fissues%2Fnew%3F%26projects%3D4%26template%3Dbug-report.yml%26assignees%3D%26labels%3Dutil%2Ccli)
-![npm](https://img.shields.io/npm/dt/%40spark-ui/cli-utils?logo=npm&labelColor=black&link=https%3A%2F%2Fwww.npmjs.com%2Fpackage%2F%40spark-ui%2Fcli-utils)
+[![storybook](https://img.shields.io/badge/storybook-black?logo=storybook)](https://sparkui.vercel.app/?path=/docs/utils-cli--docs)
+[![documentation](https://img.shields.io/badge/documentation-black?logo=googledocs)](https://sparkui-adv.vercel.app/docs/utils/cli)
+[![issue](https://img.shields.io/badge/report%20a%20bug-black?logo=openbugbounty&logoColor=red)](https://github.com/adevinta/spark/issues/new?&projects=4&template=bug-report.yml&assignees=&labels=util,cli)
+[![npm](https://img.shields.io/npm/dt/%40spark-ui/cli-utils?logo=npm&labelColor=black)](https://www.npmjs.com/package/@spark-ui/cli-utils)
 
 
 This package is part of the [`@spark-ui`](https://github.com/adevinta/spark) react-js user interface component library project.
 
-![Issues open](https://img.shields.io/github/issues-search/adevinta/spark?query=is%3Aopen%20label%3Autil%20label%3Acli&logo=openbugbounty&logoColor=red&label=issues%20open&color=red&link=https%3A%2F%2Fgithub.com%2Fadevinta%2Fspark%2Fissues%3Fq%3Dis%253Aopen%2Blabel%253Autil%2Blabel%253Acli)
-![NPM](https://img.shields.io/npm/l/%40spark-ui%2Fcli-utils)
+[![Issues open](https://img.shields.io/github/issues-search/adevinta/spark?query=is%3Aopen%20label%3Autil%20label%3Acli&logo=openbugbounty&logoColor=red&label=issues%20open&color=red)](https://github.com/adevinta/spark/issues?q=is%3Aopen+label%3Autil+label%3Acli)
+[![NPM](https://img.shields.io/npm/l/%40spark-ui%2Fcli-utils)](https://github.com/adevinta/spark/blob/main/packages/utils/cli/LICENSE.md)
--- packages/utils/cli/src/generate/templates/hook/[README.md].js
+++ packages/utils/cli/src/generate/templates/hook/[README.md].js
@@ -1,16 +1,18 @@
-export default ({ name }) => `# ${name}
+import { pascalCase } from 'pascal-case'
+
+export default ({ name }) => `# ${pascalCase(name)}
 > @spark-ui/${name}
 
-![storybook](https://img.shields.io/badge/storybook-black?logo=storybook&link=https%3A%2F%2Fsparkui.vercel.app%2F%3Fpath%3D%2Fdocs%2Fhooks-${name
+[![storybook](https://img.shields.io/badge/storybook-black?logo=storybook)](https://sparkui.vercel.app/?path=/docs/hooks-${name
   .split('-')
   .join('')}--docs)
-![documentation](https://img.shields.io/badge/documentation-black?logo=googledocs&link=https%3A%2F%2Fsparkui-adv.vercel.app%2Fdocs%2Fhook%2F${name})
-![issue](https://img.shields.io/badge/report%20a%20bug-black?logo=openbugbounty&logoColor=red&link=https%3A%2F%2Fgithub.com%2Fadevinta%2Fspark%2Fissues%2Fnew%3F%26projects%3D4%26template%3Dbug-report.yml%26assignees%3D%26labels%3Dhook%2C${name})
-![npm](https://img.shields.io/npm/dt/%40spark-ui/${name}?logo=npm&labelColor=black&link=https%3A%2F%2Fwww.npmjs.com%2Fpackage%2F%40spark-ui%2F${name})
+[![documentation](https://img.shields.io/badge/documentation-black?logo=googledocs)](https://sparkui-adv.vercel.app/docs/hooks/${name})
+[![issue](https://img.shields.io/badge/report%20a%20bug-black?logo=openbugbounty&logoColor=red)](https://github.com/adevinta/spark/issues/new?&projects=4&template=bug-report.yml&assignees=&labels=hook,${name})
+[![npm](https://img.shields.io/npm/dt/%40spark-ui/${name}?logo=npm&labelColor=black)](https://www.npmjs.com/package/@spark-ui/${name})
 
 
 This package is part of the [\`@spark-ui\`](https://github.com/adevinta/spark) react-js user interface component library project.
 
-![Issues open](https://img.shields.io/github/issues-search/adevinta/spark?query=is%3Aopen%20label%3Ahook%20label%3A${name}&logo=openbugbounty&logoColor=red&label=issues%20open&color=red&link=https%3A%2F%2Fgithub.com%2Fadevinta%2Fspark%2Fissues%3Fq%3Dis%253Aopen%2Blabel%253Ahook%2Blabel%253A${name})
-![NPM](https://img.shields.io/npm/l/%40spark-ui%2F${name})
+[![Issues open](https://img.shields.io/github/issues-search/adevinta/spark?query=is%3Aopen%20label%3Ahook%20label%3A${name}&logo=openbugbounty&logoColor=red&label=issues%20open&color=red)](https://github.com/adevinta/spark/issues?q=is%3Aopen+label%3Ahook+label%3A${name})
+[![NPM](https://img.shields.io/npm/l/%40spark-ui%2F${name})](https://github.com/adevinta/spark/blob/main/packages/hooks/${name}/LICENSE.md)
 `
