10fc9ce682960ae92871cdbbad93e4d164210eaf
soykje <jean-paul.lescouzeres@adevinta.com>
2023-10-18 13:58:42 +0200

    feat(rating): getting started with implementation, taking care of half stars (WIP)

---
+++ packages/components/rating/src/Rating.styles.ts
@@ -0,0 +1,3 @@
+import { cx } from 'class-variance-authority'
+
+export const ratingStyles = cx('flex flex-wrap gap-sm')
--- packages/components/rating/src/Rating.tsx
+++ packages/components/rating/src/Rating.tsx
@@ -1,7 +1,75 @@
-import { ComponentPropsWithoutRef, forwardRef, PropsWithChildren } from 'react'
+import { Icon } from '@spark-ui/icon'
+import { StarFill } from '@spark-ui/icons/dist/icons/StarFill'
+import { StarOutline } from '@spark-ui/icons/dist/icons/StarOutline'
+import { ComponentPropsWithoutRef, forwardRef, PropsWithChildren, useState } from 'react'
 
-export type RatingProps = ComponentPropsWithoutRef<'div'>
+import { ratingStyles } from './Rating.styles'
 
-export const Rating = forwardRef<HTMLDivElement, PropsWithChildren<RatingProps>>((props, ref) => {
-  return <div ref={ref} {...props} />
+const Star = ({ value }: { value: number }) => (
+  <div className="relative">
+    <div className="absolute z-raised overflow-hidden" style={{ width: value * 100 + '%' }}>
+      <Icon size="md">
+        <StarFill />
+      </Icon>
+    </div>
+
+    <Icon size="md">
+      <StarOutline />
+    </Icon>
+  </div>
+)
+
+export interface RatingProps extends PropsWithChildren<ComponentPropsWithoutRef<'div'>> {
+  /**
+   * The value is the number of the rating selected, on a sentiment scale from 0 to 5.
+   */
+  value: number
+  /**
+   * The stepping interval.
+   * @default 1
+   */
+  step?: number
+  /**
+   * Sets the component as interactive or not.
+   * @default false
+   */
+  readOnly?: boolean
+  /**
+   * When `true`, prevents the user from interacting.
+   * @default false
+   */
+  disabled?: boolean
+}
+
+// + intent, size
+
+export const Rating = forwardRef<HTMLDivElement, RatingProps>(({ value, ...rest }, ref) => {
+  const [ratingValue, setRatingValue] = useState(value)
+
+  const getStarValue = (value: number, index: number): number => {
+    const position = index + 1
+
+    if (position - Math.round(value) > 0) return 0
+
+    return Math.round(value * 2) / 2 >= position ? 1 : 0.5
+  }
+
+  return (
+    <div ref={ref} data-spark-component="rating" className={ratingStyles} {...rest}>
+      {Array.from({ length: 5 }).map((_, index) => (
+        <Star key={index} value={getStarValue(ratingValue, index)} />
+      ))}
+
+      {ratingValue}
+
+      <input
+        className="mt-xl block w-full"
+        type="range"
+        min="0"
+        max="5"
+        value={ratingValue}
+        onChange={e => setRatingValue(Number(e.target.value))}
+      />
+    </div>
+  )
 })
