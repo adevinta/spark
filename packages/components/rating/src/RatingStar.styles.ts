379c7e5b680240e544dd8722ce8dd9e86889fd96
acd02 <alexandre.cadiot@adevinta.com>
2023-10-20 16:57:38 +0200

    refactor(rating): clean up code

--- packages/components/rating/src/RatingStar.styles.ts
+++
@@ -1,3 +0,0 @@
-import { cva } from 'class-variance-authority'
-
-export const ratingStarVariant = cva('flex')
--- packages/components/rating/src/utils.ts
+++ packages/components/rating/src/utils.ts
@@ -1,3 +1,14 @@
-export function getNearestHalfDecimal(num: number): number {
+import { type RatingStarProps } from './RatingStar'
+
+function getNearestHalfDecimal(num: number): number {
   return Math.round(num / 0.5) * 0.5
+}
+
+export function getStarValue(ratingValue: number, index: number): RatingStarProps['value'] {
+  const starPosition = index + 1
+  const formattedValue = getNearestHalfDecimal(ratingValue)
+
+  if (Math.ceil(formattedValue) < starPosition) return 0
+
+  return formattedValue >= starPosition ? 1 : 0.5
 }
