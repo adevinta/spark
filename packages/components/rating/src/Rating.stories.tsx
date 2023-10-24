85705e9418ca36466a288fbf5854a897f1fc3251
acd02 <alexandre.cadiot@adevinta.com>
2023-10-24 14:40:41 +0200

    feat(rating): add experimental flag to the component story

--- packages/components/rating/src/Rating.stories.tsx
+++ packages/components/rating/src/Rating.stories.tsx
@@ -3,7 +3,7 @@ import { Meta, StoryFn } from '@storybook/react'
 import { Rating } from '.'
 
 const meta: Meta<typeof Rating> = {
-  title: 'Components/Rating',
+  title: 'Experimental/Rating',
   component: Rating,
 }
 
