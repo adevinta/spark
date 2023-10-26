3cef1104d207610cf75357cb7cc2b708b9d7d4db
acd02 <alexandre.cadiot@adevinta.com>
2023-10-26 18:36:55 +0200

    test(rating): add tests

--- packages/components/rating/src/Rating.test.tsx
+++ packages/components/rating/src/Rating.test.tsx
@@ -1,12 +1,96 @@
 import { render, screen } from '@testing-library/react'
-import { describe, expect, it } from 'vitest'
+import userEvent from '@testing-library/user-event'
+import { useState } from 'react'
+import { describe, expect, it, vi } from 'vitest'
 
 import { Rating } from './Rating'
 
+const utils = {
+  getInput: () =>
+    screen.getByRole('slider', {
+      name: /rating/i,
+    }),
+  getStars: (container: HTMLElement) =>
+    Array.from(container.querySelectorAll('[data-part="star"]')),
+}
+
 describe('Rating', () => {
   it('should render', () => {
-    render(<Rating />)
+    const { container } = render(<Rating defaultValue={2} />)
 
-    expect(screen.getByText(/rating/)).toBeInTheDocument()
+    expect(container.querySelector('[data-spark-component=rating]')).toBeInTheDocument()
+    expect(container.querySelectorAll('[data-part="star"]').length).toBe(5)
+  })
+
+  it('should be possible to interact', async () => {
+    const user = userEvent.setup()
+
+    const { container } = render(<Rating size="lg" defaultValue={1} />)
+
+    const stars = utils.getStars(container)
+    const input = utils.getInput()
+
+    // initial value
+    expect(input).toHaveValue('1')
+
+    await user.click(stars.at(2)!)
+    expect(input).toHaveValue('3')
+
+    await user.click(stars.at(0)!)
+    expect(input).toHaveValue('1')
+  })
+
+  it('should handle controlled mode', async () => {
+    function ControlledRating({ onValueChange }: { onValueChange: (value: number) => void }) {
+      const [value, setValue] = useState(2)
+
+      const handleInteraction = (value: number): void => {
+        onValueChange(value)
+        setValue(value)
+      }
+
+      return <Rating value={value} onValueChange={handleInteraction} />
+    }
+
+    const onValueChangeSpy = vi.fn()
+    const user = userEvent.setup()
+    const { container } = render(<ControlledRating onValueChange={onValueChangeSpy} />)
+
+    const stars = utils.getStars(container)
+    const input = utils.getInput()
+
+    // initial value
+    expect(input).toHaveValue('2')
+
+    await user.click(stars.at(4)!)
+    expect(input).toHaveValue('5')
+    expect(onValueChangeSpy).toHaveBeenCalledTimes(1)
+    expect(onValueChangeSpy).toHaveBeenCalledWith(5)
+  })
+
+  it('should not be possible to interact when in readOnly (in controlled mode)', async () => {
+    const user = userEvent.setup()
+    const handleValueChange = vi.fn()
+
+    render(<Rating value={1} onValueChange={handleValueChange} readOnly />)
+
+    const input = utils.getInput()
+
+    await user.click(input)
+    expect(handleValueChange).not.toHaveBeenCalled()
+  })
+
+  it('should not be possible to interact when disabled (in uncontrolled mode)', async () => {
+    const user = userEvent.setup()
+    const { container } = render(<Rating defaultValue={1} disabled />)
+
+    const stars = utils.getStars(container)
+    const input = utils.getInput()
+
+    expect(input).toBeDisabled()
+
+    // check that click doesn't do anything
+    await user.click(stars.at(4)!)
+    expect(input).toHaveValue('1')
   })
 })
---
+++ packages/components/rating/src/utils.test.ts
@@ -0,0 +1,66 @@
+import { describe, expect, it } from 'vitest'
+
+import { getNearestDecimal, getStarValue, splitAt } from './utils'
+
+describe('getNearestDecimal', () => {
+  it.each([
+    { value: 0, expected: 0 },
+    { value: 0.4, expected: 0 },
+    { value: 0.49, expected: 0 },
+    { value: 0.5, expected: 0 },
+    { value: 0.74, expected: 0 },
+    { value: 0.75, expected: 1 },
+
+    // Edge cases: When value is undefined
+    { value: undefined, expected: 0 },
+  ])('should return the expected decimal for: $value', ({ value, expected }) => {
+    expect(getNearestDecimal(value)).toEqual(expected)
+  })
+})
+
+describe('getStarValue', () => {
+  it.each([
+    { value: 0, index: 0, expected: 0 },
+
+    // Stars with indices less than the value should be filled
+    { value: 3, index: 0, expected: 1 },
+    { value: 3, index: 1, expected: 1 },
+    { value: 3, index: 2, expected: 1 },
+
+    // Stars with indices greater than or equal to the value should be empty
+    { value: 3, index: 3, expected: 0 },
+    { value: 3, index: 4, expected: 0 },
+
+    // Rounding cases for fractional values
+    { value: 0.1, index: 0, expected: 0 },
+    { value: 0.24, index: 0, expected: 0 },
+    { value: 0.25, index: 0, expected: 0.5 },
+    { value: 0.5, index: 0, expected: 0.5 },
+    { value: 0.74, index: 0, expected: 0.5 },
+    { value: 0.75, index: 0, expected: 1 },
+    { value: 0.9, index: 0, expected: 1 },
+    { value: 1, index: 0, expected: 1 },
+
+    // Edge cases: When value is undefined
+    { value: undefined, index: 4, expected: 0 },
+
+    // Edge cases: When value is out of range (below)
+    { value: -1, index: 4, expected: 0 },
+
+    // Edge cases: When value is out of range (above)
+    { value: 1000, index: 4, expected: 1 },
+  ])('should return the expected star value for: $value', ({ value, index, expected }) => {
+    expect(getStarValue({ value, index })).toEqual(expected)
+  })
+})
+
+describe('splitAt', () => {
+  const letters = ['a', 'b', 'c', 'd', 'e']
+
+  it('should split an array at the given index into two parts', () => {
+    expect(splitAt(letters, 2)).toStrictEqual([
+      ['a', 'b'],
+      ['c', 'd', 'e'],
+    ])
+  })
+})
