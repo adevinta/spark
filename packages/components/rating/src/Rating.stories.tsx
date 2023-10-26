431c8c753548aea55052193a9e56e9b5d1b450ba
acd02 <alexandre.cadiot@adevinta.com>
2023-10-26 10:43:23 +0200

    feat(rating): extend stories

--- packages/components/rating/src/Rating.stories.tsx
+++ packages/components/rating/src/Rating.stories.tsx
@@ -1,4 +1,5 @@
 import { Meta, StoryFn } from '@storybook/react'
+import { type ComponentProps } from 'react'
 
 import { Rating } from '.'
 
@@ -7,10 +8,37 @@ const meta: Meta<typeof Rating> = {
   component: Rating,
 }
 
+type RatingProps = ComponentProps<typeof Rating>
+
 export default meta
 
 export const Default: StoryFn = _args => (
   <div className="w-fit">
-    <Rating defaultValue={4.5} />
+    <Rating defaultValue={2.5} />
+  </div>
+)
+
+export const ReadOnly: StoryFn = _args => (
+  <div className="w-fit">
+    <Rating readOnly defaultValue={2.5} />
+  </div>
+)
+
+export const Disabled: StoryFn = _args => (
+  <div className="w-fit">
+    <Rating disabled defaultValue={2.5} />
+  </div>
+)
+
+const sizes: RatingProps['size'][] = ['sm', 'md', 'lg']
+export const Sizes: StoryFn = _args => (
+  <div className="flex flex-col flex-wrap items-start gap-md">
+    {sizes.map(size => {
+      return (
+        <div key={size} className="w-fit">
+          <Rating size={size} defaultValue={2.5} />
+        </div>
+      )
+    })}
   </div>
 )
