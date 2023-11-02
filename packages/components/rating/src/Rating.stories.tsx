574b5efefe99ab9e66b5950d37861dd29ac9419f
soykje <jean-paul.lescouzeres@adevinta.com>
2023-11-02 15:21:52 +0100

    docs(rating): adding documentation and SB stories

--- packages/components/rating/src/Rating.stories.tsx
+++ packages/components/rating/src/Rating.stories.tsx
@@ -1,6 +1,7 @@
+import { StoryLabel } from '@docs/helpers/StoryLabel'
 import { Meta, StoryFn } from '@storybook/react'
 
-import { Rating } from '.'
+import { Rating, type RatingProps } from '.'
 
 const meta: Meta<typeof Rating> = {
   title: 'Experimental/Rating',
@@ -9,4 +10,25 @@ const meta: Meta<typeof Rating> = {
 
 export default meta
 
-export const Default: StoryFn = _args => <Rating aria-label="rating" />
+const sizes: RatingProps['size'][] = ['sm', 'md', 'lg']
+
+export const Default: StoryFn = _args => <Rating defaultValue={3} aria-label="Rating control" />
+
+export const Readonly: StoryFn = _args => (
+  <Rating defaultValue={3.5} aria-label="Rating control with readOnly" readOnly />
+)
+
+export const Disabled: StoryFn = _args => (
+  <Rating defaultValue={3} aria-label="Rating control disabled" disabled />
+)
+
+export const Size: StoryFn = _args => (
+  <div className="flex flex-row flex-wrap gap-xl">
+    {sizes.map(size => (
+      <div key={size} className="">
+        <StoryLabel>{`${size}${size === 'md' ? ' (default)' : ''}`}</StoryLabel>
+        <Rating defaultValue={3} size={size} aria-label={`Rating control ${size}`} />
+      </div>
+    ))}
+  </div>
+)
