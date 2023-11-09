6e102b6f69a85ec2d8f071c65de333a8605e3f78
Powerplex <julien.bonnin.dev@gmail.com>
2023-11-09 14:16:09 +0100

    feat(select): select api

--- packages/components/select/src/index.ts
+++ packages/components/select/src/index.ts
@@ -1,1 +1,29 @@
-export { Select } from './Select'
+import type { FC } from 'react'
+
+import { Select as Root, type SelectProps } from './Select'
+import { Item } from './SelectItem'
+import { Items } from './SelectItems'
+import { ItemsGroup } from './SelectItemsGroup'
+import { LeadingIcon } from './SelectLeadingIcon'
+import { Trigger } from './SelectTrigger'
+
+export const Select: FC<SelectProps> & {
+  Trigger: typeof Trigger
+  LeadingIcon: typeof LeadingIcon
+  Items: typeof Items
+  Item: typeof Item
+  ItemsGroup: typeof ItemsGroup
+} = Object.assign(Root, {
+  Trigger,
+  LeadingIcon,
+  Items,
+  Item,
+  ItemsGroup,
+})
+
+Select.displayName = 'Select'
+Trigger.displayName = 'Select.Trigger'
+LeadingIcon.displayName = 'Select.LeadingIcon'
+Items.displayName = 'Select.Items'
+Item.displayName = 'Select.Item'
+ItemsGroup.displayName = 'Select.ItemsGroup'
