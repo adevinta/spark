598301afcf2be63878192acffcc7383895fbf64a
Andrés Alvarez <andres.alvarez@andresalvarez-MacBook-Pro-7R6H.local>
2023-11-09 15:11:34 +0100

    feat(dialog): add narrow prop

--- packages/components/dialog/src/DialogContent.tsx
+++ packages/components/dialog/src/DialogContent.tsx
@@ -4,11 +4,16 @@ import { forwardRef, type ReactElement, type Ref, useEffect } from 'react'
 import { dialogContentStyles, type DialogContentStylesProps } from './DialogContent.styles'
 import { useDialog } from './DialogContext'
 
-export type ContentProps = RadixDialog.DialogContentProps & DialogContentStylesProps
+export interface ContentProps extends RadixDialog.DialogContentProps, DialogContentStylesProps {
+  /**
+   * When set to true, the content will adjust its width to fit the content rather than taking up the full available width.
+   */
+  isNarrow?: boolean
+}
 
 export const Content = forwardRef(
   (
-    { children, className, size = 'md', ...rest }: ContentProps,
+    { children, className, isNarrow = false, size = 'md', ...rest }: ContentProps,
     ref: Ref<HTMLDivElement>
   ): ReactElement => {
     const { setIsFullScreen } = useDialog()
@@ -24,8 +29,9 @@ export const Content = forwardRef(
         data-spark-component="dialog-content"
         ref={ref}
         className={dialogContentStyles({
-          size,
           className,
+          isNarrow,
+          size,
         })}
         {...rest}
       >
