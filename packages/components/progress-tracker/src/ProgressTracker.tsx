4d23fb20c321cfa37d6d151ebcec8b871aa4661f
soykje <jean-paul.lescouzeres@adevinta.com>
2023-12-06 12:13:06 +0100

    feat(progress-tracker): add disabled and readonly features

--- packages/components/progress-tracker/src/ProgressTracker.tsx
+++ packages/components/progress-tracker/src/ProgressTracker.tsx
@@ -1,7 +1,10 @@
 import { cx } from 'class-variance-authority'
 import { type ComponentPropsWithoutRef, forwardRef, type PropsWithChildren, useState } from 'react'
 
-import { ProgressTrackerContext } from './ProgressTrackerContext'
+import {
+  ProgressTrackerContext,
+  type ProgressTrackerContextInterface,
+} from './ProgressTrackerContext'
 import type { ProgressTrackerStepVariantsProps } from './ProgressTrackerStep.styles'
 
 export interface ProgressTrackerProps
@@ -36,19 +39,28 @@ export const ProgressTracker = forwardRef<HTMLDivElement, PropsWithChildren<Prog
     },
     ref
   ) => {
-    const [steps, setSteps] = useState<Set<string>>(new Set())
+    const [steps, setSteps] = useState<ProgressTrackerContextInterface['steps']>(new Set())
     const [stepIndex, setStepIndex] = useState<number>(propStepIndex)
 
     const onStepClick = (stepId: string) => {
-      setStepIndex([...steps].indexOf(stepId))
+      const stepIndex = [...steps].findIndex(step => step.id === stepId)
+
+      setStepIndex(stepIndex)
       onStepClickProp?.(stepId)
     }
 
     const Component = readOnly ? 'div' : 'nav'
 
     return (
-      <ProgressTrackerContext.Provider value={{ stepIndex, onStepClick, steps, setSteps, size }}>
-        <Component ref={ref} className={cx('inline-flex', className)} {...rest}>
+      <ProgressTrackerContext.Provider
+        value={{ stepIndex, onStepClick, steps, setSteps, size, readOnly }}
+      >
+        <Component
+          ref={ref}
+          aria-label="progress"
+          className={cx('inline-flex', className)}
+          {...rest}
+        >
           <ol className="flex w-full flex-nowrap" style={{ counterReset: 'step' }}>
             {children}
           </ol>
