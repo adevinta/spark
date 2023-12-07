10bb43a75451f3d30fa6ee01c344c96159bb3bab
soykje <jean-paul.lescouzeres@adevinta.com>
2023-12-07 20:49:53 +0100

    feat(progress-tracker): add orientation prop

--- packages/components/progress-tracker/src/ProgressTracker.stories.tsx
+++ packages/components/progress-tracker/src/ProgressTracker.stories.tsx
@@ -11,6 +11,7 @@ const meta: Meta<typeof ProgressTracker> = {
 export default meta
 
 const sizes: ProgressTrackerProps['size'][] = ['sm', 'md', 'lg']
+const orientations: ProgressTrackerProps['orientation'][] = ['horizontal', 'vertical']
 
 export const Default: StoryFn = _args => (
   <ProgressTracker stepIndex={1} onStepClick={id => console.log('Clicked on', id)}>
@@ -45,6 +46,30 @@ export const Size: StoryFn = _args => (
           <ProgressTracker.Step label="Build" />
           <ProgressTracker.Step label="Deploy" />
           <ProgressTracker.Step label="Iterate" />
+        </ProgressTracker>
+      </div>
+    ))}
+  </div>
+)
+
+export const Orientation: StoryFn = _args => (
+  <div className="flex flex-wrap gap-lg">
+    {orientations.map(orientation => (
+      <div key={orientation}>
+        <StoryLabel>{`${orientation}${
+          orientation === 'horizontal' ? ' (default)' : ''
+        }`}</StoryLabel>
+        <ProgressTracker
+          stepIndex={1}
+          orientation={orientation as ProgressTrackerProps['orientation']}
+          {...(orientation === 'vertical' && {
+            className: 'w-[120px]',
+          })}
+        >
+          <ProgressTracker.Step label="Build" />
+          <ProgressTracker.Step label="Deploy with confidence" disabled />
+          <ProgressTracker.Step label="Iterate again and again" disabled />
+          <ProgressTracker.Step label="Repeat" />
         </ProgressTracker>
       </div>
     ))}
---
+++ packages/components/progress-tracker/src/variants/disabled.ts
@@ -0,0 +1,21 @@
+export const disabledItemVariants = {
+  disabled: {
+    true: 'before:opacity-dim-3 after:opacity-dim-3',
+    false: '',
+  },
+  disabledBefore: {
+    true: 'before:opacity-dim-3',
+    false: '',
+  },
+  disabledAfter: {
+    true: 'after:opacity-dim-3',
+    false: '',
+  },
+}
+
+export const disabledButtonVariants = {
+  disabled: {
+    true: 'before:opacity-dim-3',
+    false: '',
+  },
+}
