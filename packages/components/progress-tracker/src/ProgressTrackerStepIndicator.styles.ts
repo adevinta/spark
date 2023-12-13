a55502b1778431b2519ca2231c220e6ebf548b36
soykje <jean-paul.lescouzeres@adevinta.com>
2023-12-13 11:05:45 +0100

    style(progress-tracker): improv. indicator+label alignements

--- packages/components/progress-tracker/src/ProgressTrackerStepIndicator.styles.ts
+++ packages/components/progress-tracker/src/ProgressTrackerStepIndicator.styles.ts
@@ -2,22 +2,29 @@ import { cva } from 'class-variance-authority'
 
 export const stepIndicatorVariant = cva(
   [
-    'absolute z-base',
-    'flex shrink-0 justify-center items-center',
+    'relative flex shrink-0 justify-center items-center',
     'rounded-full border-sm',
     'text-body-2 font-bold text-on-basic-container',
     'group-disabled/btn:opacity-dim-3 group-disabled/btn:bg-transparent',
-    // Horizontal orientation
-    'group-data-[orientation=horizontal]/list:bottom-full group-data-[orientation=horizontal]/list:left-[50%] group-data-[orientation=horizontal]/list:translate-x-[-50%]',
-    // Vertical orientation
-    'group-data-[orientation=vertical]/list:right-full group-data-[orientation=vertical]/list:top-none',
   ],
   {
     variants: {
       size: {
-        sm: ['w-sz-16 h-sz-16'],
-        md: ['w-sz-24 h-sz-24'],
-        lg: ['w-sz-32 h-sz-32'],
+        sm: [
+          'w-sz-16 h-sz-16',
+          'group-data-[orientation=horizontal]/list:mt-[-16px]',
+          'group-data-[orientation=vertical]/list:ml-[-16px]',
+        ],
+        md: [
+          'w-sz-24 h-sz-24',
+          'group-data-[orientation=horizontal]/list:mt-[-24px]',
+          'group-data-[orientation=vertical]/list:ml-[-24px]',
+        ],
+        lg: [
+          'w-sz-32 h-sz-32',
+          'group-data-[orientation=horizontal]/list:mt-[-32px]',
+          'group-data-[orientation=vertical]/list:ml-[-32px]',
+        ],
       },
       state: {
         complete: [
