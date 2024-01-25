5a6c9a3418cc914ec86eae9aace043ac0f9876c0
andresin87 <andresin87@gmail.com>
2024-01-25 15:12:39 +0100

    fix(dropdown): fix text side defined

--- packages/components/dropdown/src/DropdownTrigger.styles.tsx
+++ packages/components/dropdown/src/DropdownTrigger.styles.tsx
@@ -4,6 +4,7 @@ export const styles = cva(
   [
     'flex w-full items-center justify-between',
     'min-h-sz-44 rounded-lg bg-surface text-on-surface px-lg',
+    'text-body-1',
     // outline styles
     'ring-1 outline-none ring-inset focus:ring-2',
   ],
