b05b30c0a92cf7eecc6c98e80d627b263ace13eb
Powerplex <julien.bonnin.dev@gmail.com>
2024-01-30 17:10:55 +0100

    fix(select): placeholder item for select

---
+++ packages/components/select/src/SelectPlaceholder.tsx
@@ -0,0 +1,32 @@
+import { forwardRef, type Ref, useEffect } from 'react'
+
+import { useSelectContext } from './SelectContext'
+
+export interface PlaceholderProps {
+  disabled?: boolean
+  children: string
+}
+
+export const Placeholder = forwardRef(
+  ({ disabled = false, children }: PlaceholderProps, forwardedRef: Ref<HTMLOptionElement>) => {
+    const { setPlaceholder } = useSelectContext()
+
+    useEffect(() => {
+      setPlaceholder(children)
+    }, [children])
+
+    return (
+      <option
+        data-spark-component="select-item"
+        ref={forwardedRef}
+        key="placeholder"
+        value=""
+        disabled={disabled}
+      >
+        {children}
+      </option>
+    )
+  }
+)
+
+Placeholder.displayName = 'Select.Item'
