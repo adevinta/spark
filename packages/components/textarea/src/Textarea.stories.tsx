1b75cd13d9db1c40d3a6c11da8a35da8ac46f5ae
acd02 <alexandre.cadiot@adevinta.com>
2024-04-03 17:01:34 +0200

    feat(textarea): add story showcasing customised scrollbar

--- packages/components/textarea/src/Textarea.stories.tsx
+++ packages/components/textarea/src/Textarea.stories.tsx
@@ -41,6 +41,15 @@ export const Resizable: StoryFn = () => (
   />
 )
 
+export const CustomizedScrollbar: StoryFn = () => (
+  <Textarea
+    className="[scrollbar-color:blue_transparent]"
+    rows={2}
+    aria-label="Description"
+    defaultValue="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+  />
+)
+
 export const Disabled: StoryFn = _args => (
   <div className="flex flex-col gap-md">
     <Textarea
