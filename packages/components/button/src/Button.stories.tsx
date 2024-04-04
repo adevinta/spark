fc77cc38489cf4cbb4531a0d0f85c14fe8283d40
andresin87 <andresin87@gmail.com>
2024-04-04 09:58:39 +0200

    fix(button): doc

--- packages/components/button/src/Button.stories.tsx
+++ packages/components/button/src/Button.stories.tsx
@@ -140,13 +140,6 @@ export const LoadingWithText: StoryFn = () => {
         </Icon>
         Button with long text
       </Button>
-
-      <Button isLoading={isLoading} loadingText="Loading (spinner to the right)...">
-        <Icon>
-          <FavoriteOutline />
-        </Icon>
-        Button with long text
-      </Button>
     </div>
   )
 }
