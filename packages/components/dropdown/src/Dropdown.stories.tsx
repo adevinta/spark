9b3d382811737c59b38c108eca542a05d6758e0c
acd02 <alexandre.cadiot@adevinta.com>
2024-04-12 13:49:20 +0200

    docs(dropdown): introduce specific stories dedicated for QA

--- packages/components/dropdown/src/Dropdown.stories.tsx
+++ packages/components/dropdown/src/Dropdown.stories.tsx
@@ -555,3 +555,35 @@ export const FormFieldValidation: StoryFn = () => {
     </div>
   )
 }
+
+export const QAWithButtons: StoryFn = _args => {
+  return (
+    <div className="py-[1000px]">
+      <Button className="my-sm" onClick={() => console.log('hello')}>
+        hello
+      </Button>
+      <Dropdown>
+        <Dropdown.Trigger aria-label="Book">
+          <Dropdown.LeadingIcon>
+            <BookmarkFill />
+          </Dropdown.LeadingIcon>
+          <Dropdown.Value placeholder="Pick a book" />
+        </Dropdown.Trigger>
+
+        <Dropdown.Popover>
+          <Dropdown.Items>
+            <Dropdown.Item value="book-1">To Kill a Mockingbird</Dropdown.Item>
+            <Dropdown.Item value="book-2">War and Peace</Dropdown.Item>
+            <Dropdown.Item value="book-3">The Idiot</Dropdown.Item>
+            <Dropdown.Item value="book-4">A Picture of Dorian Gray</Dropdown.Item>
+            <Dropdown.Item value="book-5">1984</Dropdown.Item>
+            <Dropdown.Item value="book-6">Pride and Prejudice</Dropdown.Item>
+          </Dropdown.Items>
+        </Dropdown.Popover>
+      </Dropdown>
+      <Button className="my-sm" onClick={() => console.log('there')}>
+        there
+      </Button>
+    </div>
+  )
+}
