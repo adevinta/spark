0c0512922f3e33b6ca4b678d917e325c952a69b7
andresin87 <andresin87@gmail.com>
2024-06-12 09:56:26 +0200

    feat(cli-utils): exoposes all configuration parameters as a build argument

--- packages/utils/cli/bin/spark-scan.mjs
+++ packages/utils/cli/bin/spark-scan.mjs
@@ -1,15 +1,21 @@
 #! /usr/bin/env node
 
 import { Command } from 'commander'
-import { adoption } from '../src/scan/index.mjs'
+import { adoption, config } from '../src/scan/index.mjs'
 
 const program = new Command()
 
 program
   .command('adoption')
   .description('Scan @spark-ui adoption for .tsx files with given imports')
-  .option('-c, --configuration <path>', 'configuration file route', '.spark-ui.cjs')
-  .option('-o, --output <path>', 'output file route')
+  .option('-c, --configuration <config>', 'configuration file route', '.spark-ui.cjs')
+  .option('-o, --output <output>', 'output file route')
+  .option('-v, --verbose', 'output log information', false)
+  .option('-d, --details', 'output information about each match', config.details)
+  .option('-s, --sort <sort>', 'sort results (count or alphabetical)', config.sort)
+  .option('-dir, --directory <directory>', 'directory to parse', config.directory)
+  .option('-ext, --extensions <extensions...>', 'file extensions to parse', config.extensions)
+  .option('-i, --imports <imports...>', 'import patterns to identify', config.imports)
   .action(adoption)
 
 program.parse(process.argv)
