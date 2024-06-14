9917041a93f8faa1fa796af2821b089b5496ff39
soykje <jean-paul.lescouzeres@adevinta.com>
2024-06-14 15:55:43 +0200

    fix(snackbar): reset data-attributes related to swipe after cancel

--- packages/components/snackbar/src/SnackbarItem.tsx
+++ packages/components/snackbar/src/SnackbarItem.tsx
@@ -95,7 +95,6 @@ export const SnackbarItem = forwardRef<HTMLDivElement, PropsWithChildren<Snackba
       onSwipeCancel: state.resumeAll,
       onSwipeEnd: ({ direction }) => {
         ;['left', 'right'].includes(`${direction}`) && state.close(toast.key)
-        state.resumeAll()
       },
     })
 
@@ -140,8 +139,10 @@ export const SnackbarItem = forwardRef<HTMLDivElement, PropsWithChildren<Snackba
         {...toastProps}
         {...rest}
         data-animation={toast.animation}
-        data-swipe={swipeState}
-        data-swipe-direction={swipeDirection}
+        {...(!(swipeState === 'cancel' && toast.animation === 'exiting') && {
+          'data-swipe': swipeState,
+          'data-swipe-direction': swipeDirection,
+        })}
         {...(toast.animation === 'exiting' && {
           // Remove snackbar when the exiting animation completes
           onAnimationEnd: () => state.remove(toast.key),
