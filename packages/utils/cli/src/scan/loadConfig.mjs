fe194e14c9318ef891846a9cc5c64c477c546753
andresin87 <andresin87@gmail.com>
2024-06-19 14:44:08 +0200

    fix(cli-utils): merge values

--- packages/utils/cli/src/scan/loadConfig.mjs
+++ packages/utils/cli/src/scan/loadConfig.mjs
@@ -1,4 +1,7 @@
 import { existsSync } from 'fs'
+import merge from 'lodash.merge'
+
+import * as defaultConfig from './config.mjs'
 
 export async function loadConfig(configFileRoute, { logger }) {
   try {
@@ -6,16 +9,24 @@ export async function loadConfig(configFileRoute, { logger }) {
       logger.info('ℹ️ Loading spark-ui custom configuration file')
       const { default: customConfig } = await import(configFileRoute)
 
-      return customConfig
+      const config = {
+        adoption: merge(defaultConfig, {
+          ...customConfig.adoption,
+          imports: customConfig.imports || defaultConfig.imports,
+          extensions: customConfig.extensions || defaultConfig.extensions,
+        }),
+      }
+
+      return config
     } else {
       logger.warn('⚠️ No custom configuration file found')
       logger.info('ℹ️ Loading default configuration')
 
-      return {}
+      return { ...defaultConfig }
     }
   } catch (error) {
     logger.error('💥 Something went wrong loading the custom configuration file')
 
-    return {}
+    return { ...defaultConfig }
   }
 }
