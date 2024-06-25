0c5300cc6cc352d6f3ef02edb368fd2853e0d568
soykje <jean-paul.lescouzeres@adevinta.com>
2024-06-25 16:28:18 +0200

    refactor(cli-utils): removes last updates about config harmonized format

--- packages/utils/cli/src/scan/config.mjs
+++ packages/utils/cli/src/scan/config.mjs
@@ -1,11 +1,7 @@
-export default {
-  adoption: {
-    details: false,
-    sort: 'count',
-    imports: ['@spark-ui'],
-    extensions: ['.tsx', '.ts'],
-    directory: '.',
-    verbose: false,
-    output: null,
-  },
-}
+export const details = false
+export const sort = 'count'
+export const imports = ['@spark-ui']
+export const extensions = ['.tsx', '.ts']
+export const directory = '.'
+export const verbose = false
+export const output = null
