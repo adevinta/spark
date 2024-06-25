f55dc53c8330eaaf74e69e4fbaceb7258e387efb
soykje <jean-paul.lescouzeres@adevinta.com>
2024-06-25 17:57:35 +0200

    test(cli-utils): adding more tests

--- packages/utils/cli/src/scan/config.cjs
+++ packages/utils/cli/src/scan/config.cjs
@@ -1,19 +1,37 @@
 module.exports = {
   adoption: {
+    /**
+     * Shows (or not) details about each package adoption
+     * @param {boolean}
+     * @default false
+     */
     details: false,
+    /**
+     * Sorts packages list, based on count or alphabetical order
+     * @param {'count'|'alphabetical'}
+     * @default 'count'
+     */
     sort: 'count',
+    /**
+     * Packages pattern to be scanned
+     * @params {string[]}
+     */
     imports: ['@spark-ui'],
+    /**
+     * File extensions to be scanned
+     * @params {string[]}
+     */
     extensions: ['.tsx', '.ts'],
+    /**
+     * Directory to be scanned
+     * @param {string}
+     * @default '.''
+     */
     directory: '.',
+    /**
+     * Output file path to export the results
+     * @param {string}
+     */
     output: null,
   },
 }
-
-/***
-- `details` (boolean) - whether to show the details of the adoption or not. Default: false
-- `sort` ('count' | 'alphabetical') - packages are sorted alphabetically. Default: false means sorted by adoption number
-- `imports` (array) - the imports to be scanned.
-- `extensions` (array) - the extensions to be scanned
-- `directory` (string) - the directory to be scanned. Default: '.' means the current directory
-- `output` (string) - the output file path to export into
-***/
