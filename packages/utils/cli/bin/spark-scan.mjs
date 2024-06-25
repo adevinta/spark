1185a68c95e9d92fea0b3733344a813cd7fea056
andresin87 <andresin87@gmail.com>
2024-06-25 16:13:03 +0200

    fix(cli-utils): add some feedback when not loading a given config file

--- packages/utils/cli/bin/spark-scan.mjs
+++ packages/utils/cli/bin/spark-scan.mjs
@@ -8,14 +8,18 @@ const program = new Command()
 program
   .command('adoption')
   .description('Scan @spark-ui adoption for .tsx files with given imports')
-  .option('-c, --configuration <config>', 'configuration file route', '.spark-ui.cjs')
+  .option('-c, --configuration <config>', 'configuration file route')
   .option('-o, --output <output>', 'output file route')
   .option('-v, --verbose', 'output log information', false)
-  .option('-d, --details', 'output information about each match', config.details)
-  .option('-s, --sort <sort>', 'sort results (count or alphabetical)', config.sort)
-  .option('-dir, --directory <directory>', 'directory to parse', config.directory)
-  .option('-ext, --extensions <extensions...>', 'file extensions to parse', config.extensions)
-  .option('-i, --imports <imports...>', 'import patterns to identify', config.imports)
+  .option('-d, --details', 'output information about each match', config.adoption.details)
+  .option('-s, --sort <sort>', 'sort results (count or alphabetical)', config.adoption.sort)
+  .option('-dir, --directory <directory>', 'directory to parse', config.adoption.directory)
+  .option(
+    '-ext, --extensions <extensions...>',
+    'file extensions to parse',
+    config.adoption.extensions
+  )
+  .option('-i, --imports <imports...>', 'import patterns to identify', config.adoption.imports)
   .action(adoption)
 
 program.parse(process.argv)
