bfa551245eeb591a03b258b251ac973046d25fb5
acd02 <alexandre.cadiot@adevinta.com>
2024-07-25 17:03:36 +0200

    feat: update Playwright config to retain videos on failure

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,6 +24,7 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    video: 'retain-on-failure',
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
