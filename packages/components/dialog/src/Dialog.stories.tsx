56d74432ddbf3a828aeb0474df5419521a4bc699
soykje <jean-paul.lescouzeres@adevinta.com>
2024-07-30 17:36:03 +0200

    test(dialog): adding QA story with default opened dialog

--- packages/components/dialog/src/Dialog.stories.tsx
+++ packages/components/dialog/src/Dialog.stories.tsx
@@ -50,6 +50,42 @@ export const Default: StoryFn = () => {
   )
 }
 
+export const QAWithDefaultOpen: StoryFn = () => {
+  return (
+    <Dialog defaultOpen>
+      <Dialog.Trigger asChild>
+        <Button>Edit profile</Button>
+      </Dialog.Trigger>
+
+      <Dialog.Portal>
+        <Dialog.Overlay />
+
+        <Dialog.Content>
+          <Dialog.Header>
+            <Dialog.Title>Edit profile</Dialog.Title>
+          </Dialog.Header>
+
+          <Dialog.Body>
+            <Dialog.Description>
+              Make changes to your profile here. Click save when you are done.
+            </Dialog.Description>
+
+            <p>Lorem ipsum dolor sit amet</p>
+          </Dialog.Body>
+
+          <Dialog.Footer className="flex justify-end gap-md">
+            <Dialog.Close asChild>
+              <Button>Close</Button>
+            </Dialog.Close>
+          </Dialog.Footer>
+
+          <Dialog.CloseButton aria-label="Close edit profile" />
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog>
+  )
+}
+
 export const Controlled: StoryFn = () => {
   const [isOpen, setIsOpen] = useState(false)
 
