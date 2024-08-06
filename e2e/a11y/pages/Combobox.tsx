1e4054d47e15d65c5cc711dcaee85f951a46d2e3
soykje <jean-paul.lescouzeres@adevinta.com>
2024-08-06 15:58:56 +0200

    test: adds combobox implementation and improves alert-dialog one

---
+++ e2e/a11y/pages/Combobox.tsx
@@ -0,0 +1,60 @@
+import { Combobox } from '@spark-ui/combobox'
+import { PenOutline } from '@spark-ui/icons/dist/icons/PenOutline'
+import React from 'react'
+
+export const A11yCombobox = () => (
+  <section>
+    <div>
+      <Combobox defaultOpen>
+        <Combobox.Trigger>
+          <Combobox.LeadingIcon>
+            <PenOutline />
+          </Combobox.LeadingIcon>
+          <Combobox.Input aria-label="Book" placeholder="Pick a book" />
+          <Combobox.ClearButton aria-label="Clear input" />
+          <Combobox.Disclosure openedLabel="Close popup" closedLabel="Open popup" />
+        </Combobox.Trigger>
+
+        <Combobox.Popover>
+          <Combobox.Items>
+            <Combobox.Empty>No results found</Combobox.Empty>
+            <Combobox.Item value="book-1">To Kill a Mockingbird</Combobox.Item>
+            <Combobox.Item value="book-2">War and Peace</Combobox.Item>
+            <Combobox.Item value="book-3">The Idiot</Combobox.Item>
+            <Combobox.Item value="book-4">A Picture of Dorian Gray</Combobox.Item>
+            <Combobox.Item value="book-5">1984</Combobox.Item>
+            <Combobox.Item value="book-6">Pride and Prejudice</Combobox.Item>
+          </Combobox.Items>
+        </Combobox.Popover>
+      </Combobox>
+    </div>
+
+    <div>
+      <Combobox defaultOpen multiple defaultValue={['book-1', 'book-2']}>
+        <Combobox.Trigger>
+          <Combobox.LeadingIcon>
+            <PenOutline />
+          </Combobox.LeadingIcon>
+          <Combobox.SelectedItems />
+          <Combobox.Input aria-label="Book" placeholder="Pick a book" />
+          <Combobox.ClearButton aria-label="Clear input" />
+          <Combobox.Disclosure openedLabel="Close popup" closedLabel="Open popup" />
+        </Combobox.Trigger>
+
+        <Combobox.Popover>
+          <Combobox.Items>
+            <Combobox.Empty>No results found</Combobox.Empty>
+            <Combobox.Item value="book-1">To Kill a Mockingbird</Combobox.Item>
+            <Combobox.Item value="book-2">War and Peace</Combobox.Item>
+            <Combobox.Item value="book-3">The Idiot</Combobox.Item>
+            <Combobox.Item value="book-4">A Picture of Dorian Gray</Combobox.Item>
+            <Combobox.Item value="book-5">1984</Combobox.Item>
+            <Combobox.Item value="book-6">
+              Pride and Prejudice but it is an extremely long title
+            </Combobox.Item>
+          </Combobox.Items>
+        </Combobox.Popover>
+      </Combobox>
+    </div>
+  </section>
+)
--- e2e/a11y/routes/elements.tsx
+++ e2e/a11y/routes/elements.tsx
@@ -6,6 +6,7 @@ import { A11yBadge } from '../pages/Badge'
 import { A11yButton } from '../pages/Button'
 import { A11yCheckbox } from '../pages/Checkbox'
 import { A11yCollapsible } from '../pages/Collapsible'
+import { A11yCombobox } from '../pages/Combobox'
 import { A11yDialog } from '../pages/Dialog'
 import { type A11yComponentsKey } from './components'
 
@@ -22,5 +23,6 @@ export const a11yElements: Record<A11yComponentsKey, ReactNode> = {
   button: <A11yButton />,
   checkbox: <A11yCheckbox />,
   collapsible: <A11yCollapsible />,
+  combobox: <A11yCombobox />,
   dialog: <A11yDialog />,
 }
