d4197ccd9144613043361e2d991ff4b51dca18da
soykje <jean-paul.lescouzeres@adevinta.com>
2024-09-06 16:52:52 +0200

    feat(stepper): getting started with global implementation

--- packages/components/stepper/src/Stepper.tsx
+++ packages/components/stepper/src/Stepper.tsx
@@ -1,7 +1,77 @@
-import { ComponentPropsWithoutRef, forwardRef, PropsWithChildren } from 'react'
+import { useButton } from '@react-aria/button'
+import { Icon } from '@spark-ui/icon'
+import { IconButton } from '@spark-ui/icon-button'
+import { Minus } from '@spark-ui/icons/dist/icons/Minus'
+import { Plus } from '@spark-ui/icons/dist/icons/Plus'
+import { Input, InputGroup } from '@spark-ui/input'
+import { forwardRef, PropsWithChildren, useRef } from 'react'
 
-export type StepperProps = ComponentPropsWithoutRef<'div'>
+import { useStepper, type UseStepperArgs } from './useStepper'
+
+export interface StepperProps
+  extends Omit<UseStepperArgs, 'inputRef' | 'validationState' | 'incrementAriaLabel'> {
+  /**
+   * The [BCP47](https://www.ietf.org/rfc/bcp/bcp47.txt) language code for the locale.
+   * @default 'fr'
+   */
+  locale?: string
+  /**
+   * A custom aria-label for the increment button.
+   */
+  incrementAriaLabel: string
+  /**
+   * A custom aria-label for the decrement button.
+   */
+  decrementAriaLabel: string
+}
 
 export const Stepper = forwardRef<HTMLDivElement, PropsWithChildren<StepperProps>>((props, ref) => {
-  return <div ref={ref} {...props} />
+  const inputRef = useRef(null)
+  const incBtnRef = useRef(null)
+  const decBtnRef = useRef(null)
+
+  const { labelProps, groupProps, inputProps, incrementButtonProps, decrementButtonProps } =
+    useStepper({ ...props, inputRef })
+
+  // Replace by compound with dedicated StepperButton!
+  const { buttonProps: incButtonProps } = useButton(incrementButtonProps, incBtnRef)
+  const { buttonProps: decButtonProps } = useButton(decrementButtonProps, decBtnRef)
+
+  return (
+    <div ref={ref}>
+      <label {...labelProps}>{props.label}</label>
+
+      <InputGroup {...groupProps}>
+        <InputGroup.LeadingAddon asChild>
+          <IconButton
+            design="ghost"
+            intent="neutral"
+            ref={decBtnRef}
+            {...decButtonProps}
+            // aria-label="caca"
+          >
+            <Icon>
+              <Minus />
+            </Icon>
+          </IconButton>
+        </InputGroup.LeadingAddon>
+
+        <Input {...inputProps} ref={inputRef} />
+
+        <InputGroup.TrailingAddon asChild>
+          <IconButton
+            design="ghost"
+            intent="neutral"
+            ref={incBtnRef}
+            {...incButtonProps}
+            aria-label="caca"
+          >
+            <Icon>
+              <Plus />
+            </Icon>
+          </IconButton>
+        </InputGroup.TrailingAddon>
+      </InputGroup>
+    </div>
+  )
 })
