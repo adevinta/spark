50d19ad09cbbc6a82413b73e299e60b5a423caa3
soykje <jean-paul.lescouzeres@adevinta.com>
2024-09-11 16:26:29 +0200

    refactor(stepper): report fwd ref bugfix to stepper implem

--- packages/components/stepper/src/Stepper.test.tsx
+++ packages/components/stepper/src/Stepper.test.tsx
@@ -1,27 +1,116 @@
-import { render, screen } from '@testing-library/react'
+import { act, fireEvent, render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { describe, expect, it, vi } from 'vitest'
 
-import { Stepper } from './Stepper'
+import { Stepper } from '.'
+
+const defaultProps = {
+  defaultValue: 0,
+  'aria-label': 'Stepper',
+  incrementAriaLabel: 'Increment',
+  decrementAriaLabel: 'Decrement',
+  onChange: vi.fn(),
+}
 
 describe('Stepper', () => {
-  it('should render', () => {
-    render(<Stepper>Hello World!</Stepper>)
+  beforeEach(() => vi.clearAllMocks())
 
-    expect(screen.getByText('Hello World!')).toBeInTheDocument()
+  it('should handle callback on changing value', async () => {
+    const user = userEvent.setup()
+
+    render(<Stepper {...defaultProps} />)
+
+    await user.click(screen.getByLabelText('Increment'))
+    expect(screen.getByRole('textbox')).toHaveValue('1')
+
+    await user.click(screen.getByLabelText('Decrement'))
+    expect(screen.getByRole('textbox')).toHaveValue('0')
+
+    expect(defaultProps.onChange).toHaveBeenCalledTimes(2)
   })
 
-  it('should trigger click event', async () => {
+  it('should not change the value if max limit has been reached', async () => {
     const user = userEvent.setup()
-    const clickEvent = vi.fn()
 
-    // Given
-    render(<div onClick={clickEvent}>Hello World!</div>)
+    render(<Stepper {...defaultProps} maxValue={5} step={5} />)
 
-    // When
-    await user.click(screen.getByText('Hello World!'))
+    await user.click(screen.getByLabelText('Increment'))
+    expect(screen.getByRole('textbox')).toHaveValue('5')
+    expect(screen.getByLabelText('Increment')).toBeDisabled()
 
-    // Then
-    expect(clickEvent).toHaveBeenCalledTimes(1)
+    await user.click(screen.getByLabelText('Increment'))
+    expect(defaultProps.onChange).toHaveBeenCalledTimes(1)
+  })
+
+  it('should not change the value if min limit has been reached', async () => {
+    const user = userEvent.setup()
+
+    render(<Stepper {...defaultProps} minValue={-10} step={5} />)
+
+    await user.click(screen.getByLabelText('Decrement'))
+    await user.click(screen.getByLabelText('Decrement'))
+
+    expect(screen.getByRole('textbox')).toHaveValue('-10')
+    expect(screen.getByLabelText('Decrement')).toBeDisabled()
+
+    await user.click(screen.getByLabelText('Decrement'))
+    expect(defaultProps.onChange).toHaveBeenCalledTimes(2)
+  })
+
+  it('should not change value nor accept interaction when disabled', async () => {
+    const user = userEvent.setup()
+
+    render(<Stepper {...defaultProps} disabled />)
+
+    await user.click(screen.getByLabelText('Increment'))
+    await user.click(screen.getByLabelText('Decrement'))
+
+    expect(defaultProps.onChange).not.toHaveBeenCalled()
+
+    expect(screen.getByLabelText('Increment')).toBeDisabled()
+    expect(screen.getByLabelText('Decrement')).toBeDisabled()
+  })
+
+  it('should change value on direct editing', async () => {
+    const user = userEvent.setup()
+
+    render(<Stepper {...defaultProps} />)
+
+    const input = screen.getByRole('textbox')
+
+    await user.type(input, '11')
+    act(() => input.blur())
+
+    expect(input).toHaveValue('11')
+  })
+
+  it('should not change value when readonly', async () => {
+    const user = userEvent.setup()
+
+    render(<Stepper {...defaultProps} readOnly />)
+
+    const input = screen.getByRole('textbox')
+
+    await user.type(input, '11')
+    act(() => input.blur())
+
+    expect(defaultProps.onChange).not.toHaveBeenCalled()
+    expect(input).toHaveValue('0')
+  })
+
+  it('should change value on scrolling up or down', () => {
+    render(<Stepper {...defaultProps} />)
+
+    const input = screen.getByRole('textbox')
+
+    act(() => input.focus())
+
+    fireEvent.wheel(input, { deltaY: 10 })
+    expect(input).toHaveValue('1')
+
+    fireEvent.wheel(input, { deltaY: -10 })
+    expect(input).toHaveValue('0')
+
+    expect(defaultProps.onChange).toHaveBeenCalledTimes(2)
   })
 })
