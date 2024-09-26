e6c2cd8040fd4df1fb58041d417315b2d7942381
acd02 <alexandre.cadiot@adevinta.com>
2024-09-26 23:14:54 +0200

    feat(pagination): tweak types

--- packages/components/pagination/src/PaginationPages.tsx
+++ packages/components/pagination/src/PaginationPages.tsx
@@ -3,14 +3,23 @@ import { ReactNode } from 'react'
 
 import { usePagination } from './PaginationContext'
 
-interface PagesProps {
-  children: (pagination: pagination.Api) => ReactNode
+// Extract the 'page' type element from the pagination API's 'pages' array.
+type PageItem = Extract<pagination.Api['pages'][number], { type: 'page' }>
+
+// Define a type that conditionally tweaks the pagination API
+// based on the generic T parameter.
+type TweakedPaginationApi<T extends 'noEllipsis' | ''> = Omit<pagination.Api, 'pages'> & {
+  pages: T extends 'noEllipsis' ? PageItem[] : pagination.Api['pages']
 }
 
-export const Pages = ({ children }: PagesProps) => {
+interface PagesProps<T extends 'noEllipsis' | ''> {
+  children: (pagination: TweakedPaginationApi<T>) => ReactNode
+}
+
+export const Pages = <T extends 'noEllipsis' | '' = ''>({ children }: PagesProps<T>) => {
   const { pagination } = usePagination()
 
-  return children(pagination)
+  return children(pagination as TweakedPaginationApi<T>)
 }
 
 Pages.displayName = 'Pagination.Pages'
