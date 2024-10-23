45912d74230e75cc2608ecdb12ea8d819100cf72
soykje <jean-paul.lescouzeres@adevinta.com>
2024-10-23 16:56:55 +0200

    refactor(skeleton): remove useless ctx from skeleton setup

--- packages/components/skeleton/src/Skeleton.tsx
+++ packages/components/skeleton/src/Skeleton.tsx
@@ -1,20 +1,14 @@
 import { ComponentPropsWithoutRef, forwardRef, PropsWithChildren } from 'react'
 
-import { SkeletonContext } from './SkeletonContext'
-
 export interface SkeletonProps extends ComponentPropsWithoutRef<'div'> {
   isAnimated?: boolean
-  // alignment?: 'left' | 'center' | 'right'
-  // gap?: string
 }
 
 export const Skeleton = forwardRef<HTMLDivElement, PropsWithChildren<SkeletonProps>>(
-  ({ isAnimated, children, ...rest }, forwardedRef) => (
-    <SkeletonContext.Provider value={{ isAnimated }}>
-      <div ref={forwardedRef} {...rest}>
-        {children}
-      </div>
-    </SkeletonContext.Provider>
+  ({ children, ...rest }, forwardedRef) => (
+    <div ref={forwardedRef} {...rest}>
+      {children}
+    </div>
   )
 )
 
