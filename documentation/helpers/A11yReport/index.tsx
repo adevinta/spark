03ef4083f8f642cb957a9582f13f2d651a35e897
soykje <jean-paul.lescouzeres@adevinta.com>
2024-10-23 16:23:52 +0200

    refactor: fix unreachable a11y report

--- documentation/helpers/A11yReport/index.tsx
+++ documentation/helpers/A11yReport/index.tsx
@@ -3,14 +3,17 @@ import type { Report } from 'e2e/a11y/utils'
 import { useEffect, useState } from 'react'
 
 export const A11yReport = ({ of }: { of: string }) => {
-  const [errors, setErrors] = useState<Report['violations']>([])
+  const [errors, setErrors] = useState<Report['violations'] | 404>()
 
   useEffect(() => {
     const fetchReport = async (name: string) => {
       const { violations }: Report = await fetch(`/a11y/a11y-report-${name}.json`)
         .then(response => response.json())
         .then(data => data[`@spark-ui/${name}`])
-        .catch(() => console.error('Unable to find accessibility report'))
+        .catch(() => {
+          console.error('Unable to find accessibility report')
+          setErrors(404)
+        })
 
       if (violations.length) setErrors(violations)
     }
@@ -18,7 +21,26 @@ export const A11yReport = ({ of }: { of: string }) => {
     fetchReport(of)
   }, [of])
 
-  return errors.length ? (
+  if (!errors || (Array.isArray(errors) && !errors.length)) {
+    return (
+      <Callout kind="success" marginY="large">
+        <p>
+          This component has been successfully tested for <strong>WCAG 2.0 levels A and AA</strong>,{' '}
+          <strong>WCAG 2.1 levels A and AA</strong> and for common accessibility best practices.
+        </p>
+      </Callout>
+    )
+  }
+
+  if (errors === 404) {
+    return (
+      <Callout kind="error" marginY="large">
+        <p>No accessibility report were find for this component.</p>
+      </Callout>
+    )
+  }
+
+  return (
     <Callout kind="error" marginY="large">
       <p>
         This component didn&apos;t pass all tests for <strong>WCAG 2.0 levels A and AA</strong>,{' '}
@@ -37,13 +59,6 @@ export const A11yReport = ({ of }: { of: string }) => {
           </li>
         ))}
       </ul>
-    </Callout>
-  ) : (
-    <Callout kind="success" marginY="large">
-      <p>
-        This component has been successfully tested for <strong>WCAG 2.0 levels A and AA</strong>,{' '}
-        <strong>WCAG 2.1 levels A and AA</strong> and for common accessibility best practices.
-      </p>
     </Callout>
   )
 }
