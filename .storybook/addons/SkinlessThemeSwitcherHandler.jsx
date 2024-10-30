ea351163e0c34cd74c352be55d95a24269208dc9
acd02 <alexandre.cadiot@adevinta.com>
2024-10-30 14:36:23 +0100

    fix: prevent removal of anchors from URLs upon page load

--- .storybook/addons/SkinlessThemeSwitcherHandler.jsx
+++ .storybook/addons/SkinlessThemeSwitcherHandler.jsx
@@ -1,19 +1,61 @@
-import { memo, useEffect } from "react"
+import { memo, useEffect, useSyncExternalStore, useRef } from "react"
+
+/*
+  1. Refer to this issue for the context behind this:
+     https://github.com/adevinta/spark/pull/2497
+*/
 
 import { useGlobals } from "@storybook/manager-api"
 
+
+function useUrlChange() {
+  /* 1 */
+  return useSyncExternalStore(
+    (callback) => {
+      window.addEventListener("popstate", callback)
+
+      return () => {
+        window.removeEventListener("popstate", callback)
+      }
+    },
+    () => window.location.href,
+  )
+}
+
 export const SkinlessThemeSwitcherHandler = memo(function SkinlessThemeSwitcherHandler() {
   const [globals] = useGlobals()
+
   const theme = globals?.theme
+  const url = useUrlChange()
+  const initialUrlHashRef = useRef("")
+  const initialUrlSearchRef = useRef("")
+
+  useEffect(() => {
+    /* 1 */
+    initialUrlHashRef.current = window.location.hash
+    initialUrlSearchRef.current = window.location.search
+  }, [])
+
+  useEffect(() => {
+    /* 1 */
+    const isNotInitialPage = initialUrlSearchRef.current !== window.location.search
+    const isNotInitialHash =
+      window.location.hash.length && window.location.hash !== initialUrlHashRef.current
+
+    if (isNotInitialPage || isNotInitialHash) return
+
+    const newUrl = new URL(window.location)
+    newUrl.hash = initialUrlHashRef.current
+    window.history.replaceState(null, "", newUrl)
+  }, [url])
 
   useEffect(() => {
     if (!theme) return
 
     const htmlElement = document.querySelector("html")
-    if (htmlElement)  {
+    if (htmlElement) {
       htmlElement.setAttribute("data-theme", theme)
     }
-
     const iframe = document.querySelector("#storybook-preview-iframe")
     if (iframe) {
       const iframeHtmlElement = iframe.contentDocument?.documentElement
