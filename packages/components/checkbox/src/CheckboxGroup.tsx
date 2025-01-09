6232c2c6a69062dfb7d7fd00f7e5728b3cf30d64
Powerplex <julien.bonnin.dev@gmail.com>
2025-01-09 17:54:48 +0100

    refactor(checkbox): removed forwardRef from checkbox

--- packages/components/checkbox/src/CheckboxGroup.tsx
+++ packages/components/checkbox/src/CheckboxGroup.tsx
@@ -1,6 +1,6 @@
 import { useFormFieldControl } from '@spark-ui/form-field'
 import { useCombinedState } from '@spark-ui/use-combined-state'
-import { ComponentPropsWithoutRef, forwardRef, useEffect, useMemo, useRef } from 'react'
+import { ComponentPropsWithoutRef, RefObject, useEffect, useMemo, useRef } from 'react'
 
 import { checkboxGroupStyles, CheckboxGroupStylesProps } from './CheckboxGroup.styles'
 import { CheckboxGroupContext, CheckboxGroupContextState } from './CheckboxGroupContext'
@@ -17,76 +17,73 @@ export interface CheckboxGroupProps
    * The callback fired when any children Checkbox is checked or unchecked
    */
   onCheckedChange?: (value: string[]) => void
+  ref?: RefObject<HTMLDivElement>
 }
 
-export const CheckboxGroup = forwardRef<HTMLDivElement, CheckboxGroupProps>(
-  (
-    {
-      name: nameProp,
-      value: valueProp,
-      defaultValue,
-      className,
+export const CheckboxGroup = ({
+  name: nameProp,
+  value: valueProp,
+  defaultValue,
+  className,
+  intent,
+  orientation = 'vertical',
+  onCheckedChange: onCheckedChangeProp,
+  reverse = false,
+  children,
+  ref,
+  ...others
+}: CheckboxGroupProps) => {
+  const [value, setValue] = useCombinedState(valueProp, defaultValue)
+  const field = useFormFieldControl()
+  const onCheckedChangeRef = useRef(onCheckedChangeProp)
+
+  const { id, labelId, description, state, isInvalid, isRequired } = field
+  const name = nameProp ?? field.name
+
+  const current = useMemo(() => {
+    const handleCheckedChange = (checked: boolean, changed: string) => {
+      const values = value || []
+      const modified = checked ? [...values, changed] : values.filter(val => val !== changed)
+
+      setValue(modified)
+
+      if (onCheckedChangeRef.current) {
+        onCheckedChangeRef.current(modified)
+      }
+    }
+
+    return {
+      id,
+      name,
+      value,
       intent,
-      orientation = 'vertical',
-      onCheckedChange: onCheckedChangeProp,
-      reverse = false,
-      children,
-      ...others
-    },
-    ref
-  ) => {
-    const [value, setValue] = useCombinedState(valueProp, defaultValue)
-    const field = useFormFieldControl()
-    const onCheckedChangeRef = useRef(onCheckedChangeProp)
+      state,
+      isInvalid,
+      description,
+      isRequired,
+      reverse,
+      onCheckedChange: handleCheckedChange,
+    }
+  }, [id, name, value, intent, state, isInvalid, description, isRequired, setValue, reverse])
 
-    const { id, labelId, description, state, isInvalid, isRequired } = field
-    const name = nameProp ?? field.name
+  useEffect(() => {
+    onCheckedChangeRef.current = onCheckedChangeProp
+  }, [onCheckedChangeProp])
 
-    const current = useMemo(() => {
-      const handleCheckedChange = (checked: boolean, changed: string) => {
-        const values = value || []
-        const modified = checked ? [...values, changed] : values.filter(val => val !== changed)
-
-        setValue(modified)
-
-        if (onCheckedChangeRef.current) {
-          onCheckedChangeRef.current(modified)
-        }
-      }
-
-      return {
-        id,
-        name,
-        value,
-        intent,
-        state,
-        isInvalid,
-        description,
-        isRequired,
-        reverse,
-        onCheckedChange: handleCheckedChange,
-      }
-    }, [id, name, value, intent, state, isInvalid, description, isRequired, setValue, reverse])
-
-    useEffect(() => {
-      onCheckedChangeRef.current = onCheckedChangeProp
-    }, [onCheckedChangeProp])
-
-    return (
-      <CheckboxGroupContext.Provider value={current}>
-        <div
-          ref={ref}
-          className={checkboxGroupStyles({ className, orientation })}
-          role="group"
-          aria-labelledby={labelId}
-          aria-describedby={description}
-          {...others}
-        >
-          {children}
-        </div>
-      </CheckboxGroupContext.Provider>
-    )
-  }
-)
+  return (
+    <CheckboxGroupContext.Provider value={current}>
+      <div
+        ref={ref}
+        className={checkboxGroupStyles({ className, orientation })}
+        role="group"
+        aria-labelledby={labelId}
+        aria-describedby={description}
+        {...others}
+      >
+        {children}
+      </div>
+    </CheckboxGroupContext.Provider>
+  )
+}
 
 CheckboxGroup.displayName = 'CheckboxGroup'
