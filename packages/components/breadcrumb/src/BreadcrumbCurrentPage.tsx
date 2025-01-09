dd0fe8f72559586b6dc619b9df6c4a448da4fc39
Powerplex <julien.bonnin.dev@gmail.com>
2025-01-09 17:37:37 +0100

    refactor(breadcrumb): removed forwardRef from Breadcrumb

--- packages/components/breadcrumb/src/BreadcrumbCurrentPage.tsx
+++ packages/components/breadcrumb/src/BreadcrumbCurrentPage.tsx
@@ -1,34 +1,37 @@
 import { Slot } from '@spark-ui/slot'
 import { TextLink } from '@spark-ui/text-link'
 import { cx } from 'class-variance-authority'
-import { ComponentPropsWithoutRef, forwardRef } from 'react'
+import { ComponentPropsWithoutRef, RefObject } from 'react'
 
 export interface CurrentPageProps extends ComponentPropsWithoutRef<typeof TextLink> {
   asChild?: boolean
   className?: string
+  ref?: RefObject<HTMLAnchorElement>
 }
 
-export const CurrentPage = forwardRef<HTMLAnchorElement, CurrentPageProps>(
-  ({ asChild = false, className, children, ...rest }, ref) => {
-    const Component = asChild ? Slot : 'span'
+export const CurrentPage = ({
+  asChild = false,
+  className,
+  children,
+  ...rest
+}: CurrentPageProps) => {
+  const Component = asChild ? Slot : 'span'
 
-    return (
-      <Component
-        data-spark-component="breadcrumb-current-page"
-        role="link"
-        aria-disabled
-        aria-current="page"
-        className={cx(
-          '!inline overflow-hidden text-ellipsis whitespace-nowrap font-bold text-current',
-          className
-        )}
-        ref={ref}
-        {...rest}
-      >
-        {children}
-      </Component>
-    )
-  }
-)
+  return (
+    <Component
+      data-spark-component="breadcrumb-current-page"
+      role="link"
+      aria-disabled
+      aria-current="page"
+      className={cx(
+        '!inline overflow-hidden text-ellipsis whitespace-nowrap font-bold text-current',
+        className
+      )}
+      {...rest}
+    >
+      {children}
+    </Component>
+  )
+}
 
 CurrentPage.displayName = 'Breadcrumb.CurrentPage'
--- packages/components/breadcrumb/src/BreadcrumbItem.tsx
+++ packages/components/breadcrumb/src/BreadcrumbItem.tsx
@@ -1,19 +1,19 @@
 import { cx } from 'class-variance-authority'
-import { ComponentPropsWithoutRef, forwardRef } from 'react'
+import { ComponentPropsWithoutRef, RefObject } from 'react'
 
 export interface ItemProps extends ComponentPropsWithoutRef<'li'> {
   className?: string
+  ref?: RefObject<HTMLLIElement>
 }
 
-export const Item = forwardRef<HTMLLIElement, ItemProps>(({ className, ...rest }, ref) => {
+export const Item = ({ className, ...rest }: ItemProps) => {
   return (
     <li
       data-spark-component="breadcrumb-item"
-      ref={ref}
       className={cx('inline-flex min-w-none items-center gap-sm', className)}
       {...rest}
     />
   )
-})
+}
 
 Item.displayName = 'Breadcrumb.Item'
