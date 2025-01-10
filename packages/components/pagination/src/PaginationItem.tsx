20cb70e9199721aa3746bbe8b60670c61f10047a
Powerplex <julien.bonnin.dev@gmail.com>
2025-01-10 11:17:07 +0100

    refactor(pagination): removed forwardRef from pagination

--- packages/components/pagination/src/PaginationItem.tsx
+++ packages/components/pagination/src/PaginationItem.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@spark-ui/button'
 import { mergeProps } from '@zag-js/react'
-import { ComponentPropsWithoutRef, forwardRef } from 'react'
+import { ComponentPropsWithoutRef, RefObject } from 'react'
 
 import { usePagination } from './PaginationContext'
 
@@ -14,40 +14,39 @@ interface ButtonProps extends ComponentPropsWithoutRef<'button'> {
 export type ItemProps = Omit<AnchorProps | ButtonProps, 'aria-label'> & {
   'aria-label': string
   value: number
+  ref?: RefObject<HTMLButtonElement>
 }
 
-export const Item = forwardRef<HTMLButtonElement, ItemProps>(
-  ({ children, value, className, href, ...props }, ref) => {
-    const { pagination } = usePagination()
+export const Item = ({ children, value, className, href, ref, ...props }: ItemProps) => {
+  const { pagination } = usePagination()
 
-    // ZagJS props
-    const apiProps = pagination.getItemProps({ type: 'page', value })
+  // ZagJS props
+  const apiProps = pagination.getItemProps({ type: 'page', value })
 
-    // Locally managed props
-    const localProps = {
-      'data-spark-component': 'pagination-item',
-      intent: 'support',
-      design: apiProps['aria-current'] === 'page' ? 'filled' : 'ghost',
-      className,
-      ...props,
-    }
-
-    const mergedProps = mergeProps(apiProps, localProps)
-
-    return (
-      <li>
-        {href ? (
-          <Button ref={ref} {...mergedProps} asChild>
-            <a href={href}>{children || value}</a>
-          </Button>
-        ) : (
-          <Button ref={ref} {...mergedProps}>
-            {children || value}
-          </Button>
-        )}
-      </li>
-    )
+  // Locally managed props
+  const localProps = {
+    'data-spark-component': 'pagination-item',
+    intent: 'support',
+    design: apiProps['aria-current'] === 'page' ? 'filled' : 'ghost',
+    className,
+    ...props,
   }
-)
+
+  const mergedProps = mergeProps(apiProps, localProps)
+
+  return (
+    <li>
+      {href ? (
+        <Button ref={ref} {...mergedProps} asChild>
+          <a href={href}>{children || value}</a>
+        </Button>
+      ) : (
+        <Button ref={ref} {...mergedProps}>
+          {children || value}
+        </Button>
+      )}
+    </li>
+  )
+}
 
 Item.displayName = 'Pagination.Item'
--- packages/components/pagination/src/PaginationPrevTrigger.tsx
+++ packages/components/pagination/src/PaginationPrevTrigger.tsx
@@ -2,7 +2,7 @@ import { Icon } from '@spark-ui/icon'
 import { IconButton } from '@spark-ui/icon-button'
 import { ArrowVerticalLeft } from '@spark-ui/icons/dist/icons/ArrowVerticalLeft'
 import { mergeProps } from '@zag-js/react'
-import { ComponentPropsWithoutRef, forwardRef } from 'react'
+import { ComponentPropsWithoutRef, RefObject } from 'react'
 
 import { usePagination } from './PaginationContext'
 
@@ -15,61 +15,60 @@ interface ButtonProps extends ComponentPropsWithoutRef<'button'> {
 }
 export type PrevTriggerProps = Omit<AnchorProps | ButtonProps, 'aria-label'> & {
   'aria-label': string
+  ref?: RefObject<HTMLButtonElement>
 }
 
-export const PrevTrigger = forwardRef<HTMLButtonElement, PrevTriggerProps>(
-  ({ children, className, href, ...props }, ref) => {
-    const { pagination, type } = usePagination()
+export const PrevTrigger = ({ children, className, href, ref, ...props }: PrevTriggerProps) => {
+  const { pagination, type } = usePagination()
 
-    // ZagJS props
-    const apiProps = pagination.getPrevTriggerProps()
+  // ZagJS props
+  const apiProps = pagination.getPrevTriggerProps()
 
-    const shouldDisableLink =
-      type === 'link' &&
-      (apiProps as typeof apiProps & { 'data-disabled'?: string })['data-disabled'] === ''
+  const shouldDisableLink =
+    type === 'link' &&
+    (apiProps as typeof apiProps & { 'data-disabled'?: string })['data-disabled'] === ''
 
-    // Locally managed props
-    const localProps = {
-      'data-spark-component': 'pagination-prev-trigger',
-      intent: 'support',
-      design: 'ghost',
-      ...props,
-      className,
-      ...(shouldDisableLink && {
-        disabled: true,
-        role: 'link',
-        'aria-disabled': true,
-      }),
-    }
-
-    // We know 'aria-label' is included in props
-    type WithAriaLabel = Omit<typeof apiProps, 'aria-label'> & { 'aria-label': string }
-
-    const mergedProps = mergeProps(
-      apiProps,
-      localProps as unknown as typeof apiProps
-    ) as WithAriaLabel
-
-    const content = children || (
-      <Icon>
-        <ArrowVerticalLeft />
-      </Icon>
-    )
-
-    return (
-      <li>
-        {href ? (
-          <IconButton ref={ref} {...mergedProps} asChild>
-            <a href={shouldDisableLink ? undefined : href}>{content}</a>
-          </IconButton>
-        ) : (
-          <IconButton ref={ref} {...mergedProps}>
-            {content}
-          </IconButton>
-        )}
-      </li>
-    )
+  // Locally managed props
+  const localProps = {
+    'data-spark-component': 'pagination-prev-trigger',
+    intent: 'support',
+    design: 'ghost',
+    ...props,
+    className,
+    ...(shouldDisableLink && {
+      disabled: true,
+      role: 'link',
+      'aria-disabled': true,
+    }),
   }
-)
+
+  // We know 'aria-label' is included in props
+  type WithAriaLabel = Omit<typeof apiProps, 'aria-label'> & { 'aria-label': string }
+
+  const mergedProps = mergeProps(
+    apiProps,
+    localProps as unknown as typeof apiProps
+  ) as WithAriaLabel
+
+  const content = children || (
+    <Icon>
+      <ArrowVerticalLeft />
+    </Icon>
+  )
+
+  return (
+    <li>
+      {href ? (
+        <IconButton ref={ref} {...mergedProps} asChild>
+          <a href={shouldDisableLink ? undefined : href}>{content}</a>
+        </IconButton>
+      ) : (
+        <IconButton ref={ref} {...mergedProps}>
+          {content}
+        </IconButton>
+      )}
+    </li>
+  )
+}
 
 PrevTrigger.displayName = 'Pagination.PrevTrigger'
